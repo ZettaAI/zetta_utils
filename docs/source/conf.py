ad5ec32d902928121f2283254ec5ed2197c9e84c
Sergiy Popovych <sergiy.popovich@gmail.com>
2022-09-20 00:41:40 +0000

    doc source code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
     "sphinx_autodoc_typehints",
     "notfound.extension",
     "sphinx_copybutton",

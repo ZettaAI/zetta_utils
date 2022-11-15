6235a10eaa170450d6580a3058a4f839cf863824
supersergiy <sergiy.popovich@gmail.com>
2022-11-15 14:12:08 -0800

    Z blocks division (#98)

--- zetta_utils/log.py
+++ zetta_utils/log.py
@@ -5,6 +5,9 @@ from __future__ import annotations
 import logging
 
 from rich.logging import RichHandler
+from rich.traceback import install
+
+install(show_locals=False)
 
 
 def get_time_str(log_time):
@@ -12,6 +15,7 @@ def get_time_str(log_time):
 
 
 SAVED_LEVEL = "ERROR"
+
 
 def configure_logger(level=None, third_party_level="WARN"):
     for _ in (
@@ -59,11 +63,13 @@ def get_logger(name):
     configure_logger()
     return logging.getLogger(name)
 
+
 def set_verbosity(verbosity_level):
     global SAVED_LEVEL
     SAVED_LEVEL = verbosity_level
     logging.getLogger("zetta_utils").setLevel(verbosity_level)
     logging.getLogger("mazepa").setLevel(verbosity_level)
 
+
 configure_logger()
-#logger = logging.getLogger("zetta_utils")
+# logger = logging.getLogger("zetta_utils")

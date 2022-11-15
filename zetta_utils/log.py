b1b13e556ef8e234785961aeb705f887530dca76
Sergiy Popovych <sergiy.popovich@gmail.com>
2022-11-15 16:45:11 +0000

    isort on everything

--- zetta_utils/log.py
+++ zetta_utils/log.py
@@ -1,18 +1,18 @@
 """Output logging."""
 # pylint: disable=global-statement
-from __future__ import annotations
 
 import logging
 
 from rich.logging import RichHandler
 
 
+from __future__ import annotations
+
 def get_time_str(log_time):
     return log_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
 
 
 SAVED_LEVEL = "ERROR"
-
 
 def configure_logger(level=None, third_party_level="WARN"):
     for _ in (
@@ -60,13 +60,11 @@ def get_logger(name):
     configure_logger()
     return logging.getLogger(name)
 
-
 def set_verbosity(verbosity_level):
     global SAVED_LEVEL
     SAVED_LEVEL = verbosity_level
     logging.getLogger("zetta_utils").setLevel(verbosity_level)
     logging.getLogger("mazepa").setLevel(verbosity_level)
 
-
 configure_logger()
-# logger = logging.getLogger("zetta_utils")
+#logger = logging.getLogger("zetta_utils")

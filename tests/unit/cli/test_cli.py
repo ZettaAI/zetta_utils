a3d2ffcd3343d226eee6a25744ca639d8c7f36a2
Sergiy Popovych <sergiy.popovich@gmail.com>
2022-11-15 16:12:24 +0000

    Verbosity improvement

--- tests/unit/cli/test_cli.py
+++ tests/unit/cli/test_cli.py
@@ -1,6 +1,4 @@
 # pylint: disable=unused-argument,redefined-outer-name
-import json
-
 import pytest
 from click.testing import CliRunner
 
@@ -22,16 +20,14 @@ def register_dummy():
     [
         [
             {"@type": "dummy", "i": {"a": "b"}},
-            {"a": "b"},
         ],
     ],
 )
-def test_zetta_run(spec, expected_output, register_dummy, mocker):
+def test_zetta_run(spec, register_dummy, mocker):
     mocker.patch("zetta_utils.parsing.cue.load", return_value=spec)
     runner = CliRunner()
     result = runner.invoke(cli.run, ".")
     assert result.exit_code == 0
-    assert json.loads(result.output.replace("'", '"')) == expected_output
 
 
 def test_show_registry(register_dummy):

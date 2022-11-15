77e8132fc5a5016f13d6d592247a1d8589a6c46c
Sergiy Popovych <sergiy.popovich@gmail.com>
2022-11-15 16:52:16 +0000

    isort on everything

--- zetta_utils/log.py
+++ zetta_utils/log.py
@@ -1,11 +1,11 @@
 """Output logging."""
 # pylint: disable=global-statement
-
 from __future__ import annotations
 
+from rich.logging import RichHandler
 import logging
 
-from rich.logging import RichHandler
+
 
 
 def get_time_str(log_time):

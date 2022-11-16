cd1c556094a62253e94e3b868d6cb645da2233c2
supersergiy <sergiy.popovich@gmail.com>
2022-11-16 11:54:45 -0800

    Allow propper 2D field interpolation (#104)

--- tests/unit/layer/volumetric/test_build.py
+++ tests/unit/layer/volumetric/test_build.py
@@ -13,7 +13,7 @@ def test_build_exc(mocker):
 
 def test_data_resolution_read_interp(mocker):
     backend = mocker.MagicMock()
-    backend.read = mocker.MagicMock(return_value=np.ones((1, 2, 2, 2)) * 2)
+    backend.read = mocker.MagicMock(return_value=np.ones((2, 2, 2, 2)) * 2)
 
     layer = build_volumetric_layer(
         backend,
@@ -33,7 +33,7 @@ def test_data_resolution_read_interp(mocker):
     )
     np.testing.assert_array_equal(
         read_data,
-        np.ones((1, 1, 1, 1)),
+        np.ones((2, 1, 1, 1)),
     )
 
 
@@ -54,9 +54,9 @@ def test_data_resolution_write_interp(mocker):
         bcube=BoundingCube.from_slices((slice(0, 3), slice(0, 3), slice(0, 3))),
     )
 
-    layer[0:1, 0:1, 0:1] = np.ones((1, 1, 1, 1))
+    layer[0:1, 0:1, 0:1] = np.ones((2, 1, 1, 1))
     assert backend.write.call_args.kwargs["idx"] == idx
     np.testing.assert_array_equal(
         backend.write.call_args.kwargs["value"],
-        np.ones((1, 2, 2, 2)) * 2,
+        np.ones((2, 2, 2, 2)) * 2,
     )

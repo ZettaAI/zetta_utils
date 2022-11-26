0705aed5ce20e9c2d14b1cca22dd9c30d8be2318
Akhilesh Halageri <akhilesh@zetta.ai>
2022-11-26 22:05:56 +0000

    feat: convert annotationbbox to bcube

--- specs/akhilesh/get_z_blocks_x0.cue
+++ specs/akhilesh/get_z_blocks_x0.cue
@@ -1,9 +1,2 @@
 "@type": "get_z_blocks"
-bcube: {
-	"@type": "BoundingCube"
-	start_coord: [0, 0, 0]
-	end_coord: [1024, 1024, 100]
-	resolution: [4, 4, 40]
-}
-block_size:     15
-block_size_res: 40
+remote_layer: "remote/zfish_10132022_cutout_misalignments"
--- zetta_utils/parsing/ngl_state.py
+++ zetta_utils/parsing/ngl_state.py
@@ -1,18 +1,23 @@
 """neuroglancer state parsing."""
 
 from os import environ
+from typing import List
 
-from cloudfiles import CloudFiles
-from neuroglancer.viewer_state import Layer
-from neuroglancer.viewer_state import make_layer
+from neuroglancer.viewer_state import AnnotationLayer
 
+from ..bcube import BoundingCube
 from ..log import get_logger
 
 
 logger = get_logger("zetta_utils")
 remote_path = environ.get("REMOTE_LAYERS_PATH", "gs://remote-annotations")
+RESOLUTION_KEY = "voxelSize"
 
-def load(layer_name: str) -> Layer:
+
+def load(layer_name: str) -> AnnotationLayer:
+    from cloudfiles import CloudFiles
+    from neuroglancer.viewer_state import make_layer
+
     logger.info(f"Remote layer: {remote_path}/{layer_name}.")
 
     cf = CloudFiles(remote_path)
@@ -20,3 +25,20 @@ def load(layer_name: str) -> Layer:
     logger.debug(layer_json)
     return make_layer(layer_json)
 
+
+def get_bcubes_from_annotations(layer: AnnotationLayer) -> List[BoundingCube]:
+    from ..typing import Vec3D
+
+    bcubes = []
+    resolution: Vec3D = layer.to_json()[RESOLUTION_KEY]
+    for annotation in layer.annotations:
+        try:
+            bcube = BoundingCube.from_coords(
+                annotation.point_a,
+                annotation.point_b,
+                resolution=resolution,
+            )
+            bcubes.append(bcube)
+        except AttributeError:
+            ...
+    return bcubes

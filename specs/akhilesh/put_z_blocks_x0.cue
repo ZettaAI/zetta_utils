688211f19cbb37b076f01060716009d8296e43c9
Akhilesh Halageri <akhilesh@zetta.ai>
2022-11-27 22:03:00 +0000

    wip: write interface

---
+++ specs/akhilesh/put_z_blocks_x0.cue
@@ -0,0 +1,2 @@
+"@type": "put_z_blocks"
+remote_layer: "remote/zfish_10132022_cutout_misalignments_test"
--- zetta_utils/alignment/z_blocks.py
+++ zetta_utils/alignment/z_blocks.py
@@ -9,9 +9,14 @@ logger = log.get_logger("zetta_utils")
 
 
 @builder.register("get_z_blocks")
-def get_z_blocks(
-    remote_layer: str
-) -> List[Union[BoundingCube, Vec3D]]:
+def get_z_blocks(remote_layer: str) -> List[Union[BoundingCube, Vec3D]]:
     result = ngl_state.read_remote_annotations(remote_layer)
     logger.info(f"Final number of blocks/points: {len(result)}")
     return result
+
+
+@builder.register("put_z_blocks")
+def put_z_blocks(remote_layer: str) -> None:
+    resolution = [4,4,30]
+    bcubes_or_points = ngl_state.read_remote_annotations("remote/zfish_10132022_cutout_misalignments")
+    ngl_state.write_remote_annotations(remote_layer, resolution, bcubes_or_points)

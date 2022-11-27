9476d700c1b780157d90eb091b6b6e2b45dd2bed
Akhilesh Halageri <akhilesh@zetta.ai>
2022-11-27 18:30:54 +0000

    feat: ngl state read interface

--- zetta_utils/parsing/ngl_state.py
+++ zetta_utils/parsing/ngl_state.py
@@ -1,10 +1,15 @@
 """neuroglancer state parsing."""
 
 from os import environ
-from typing import List
+from typing import List, Union
 
 from cloudfiles import CloudFiles
-from neuroglancer.viewer_state import AnnotationLayer, make_layer
+from neuroglancer.viewer_state import (
+    AnnotationLayer,
+    AxisAlignedBoundingBoxAnnotation,
+    PointAnnotation,
+    make_layer,
+)
 
 from zetta_utils.bcube import BoundingCube
 from zetta_utils.log import get_logger
@@ -15,26 +20,37 @@ remote_path = environ.get("REMOTE_LAYERS_PATH", "gs://remote-annotations")
 RESOLUTION_KEY = "voxelSize"
 
 
-def load(layer_name: str) -> AnnotationLayer:
+def read_remote_annotations(layer_name: str) -> AnnotationLayer:
     logger.info(f"Remote layer: {remote_path}/{layer_name}.")
 
     cf = CloudFiles(remote_path)
     layer_json = cf.get_json(layer_name)
     logger.debug(layer_json)
-    return make_layer(layer_json)
+    layer: AnnotationLayer = make_layer(layer_json)
+
+    logger.info(f"Layer type: {layer.type}; Total: {len(layer.annotations)}.")
+    return _parse_annotations(layer)
 
 
-def get_bcubes_from_annotations(layer: AnnotationLayer) -> List[BoundingCube]:
-    bcubes = []
+def _parse_annotations(layer: AnnotationLayer) -> List[Union[BoundingCube, Vec3D]]:
+    result = []
     resolution: Vec3D = layer.to_json()[RESOLUTION_KEY]
     for annotation in layer.annotations:
+        assert isinstance(
+            annotation, (AxisAlignedBoundingBoxAnnotation, PointAnnotation)
+        ), "Annotation type not supported."
         try:
             bcube = BoundingCube.from_coords(
                 annotation.point_a,
                 annotation.point_b,
                 resolution=resolution,
             )
-            bcubes.append(bcube)
+            result.append(bcube)
         except AttributeError:
-            ...
-    return bcubes
+            point: Vec3D = annotation.point
+            result.append(point)
+    return result
+
+
+def write_remote_annotations():
+    ...

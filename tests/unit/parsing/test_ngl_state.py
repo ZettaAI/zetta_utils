083ab4b3ba5a80f8f7ae5a785e6ce59965c41b7f
Akhilesh Halageri <akhilesh@zetta.ai>
2022-12-04 18:33:02 +0000

    simplify pr 118

---
+++ tests/unit/parsing/test_ngl_state.py
@@ -0,0 +1,37 @@
+# pylint: disable=missing-docstring,unspecified-encoding,invalid-name
+import os
+import pathlib
+
+from cloudfiles import CloudFiles
+
+from zetta_utils.bcube import BoundingCube
+from zetta_utils.parsing import ngl_state
+
+PARENT_DIR = pathlib.Path(__file__).parents[1]
+TEST_DATA_DIR = "assets/remote_layers/"
+TEST_READ_FILE = "ngl_layer.json"
+TEST_WRITE_FILE = "ngl_layer_write.json"
+REMOTE_LAYERS_PATH = f"file://{PARENT_DIR}/{TEST_DATA_DIR}"
+os.environ["REMOTE_LAYERS_PATH"] = REMOTE_LAYERS_PATH
+
+
+def test_read_remote_annotations():
+    result = ngl_state.read_remote_annotations(TEST_READ_FILE)
+    assert len(result) == 2
+
+
+def test_write_remote_annotations():
+    start_coord = [52632.3828125, 73167.4140625, 1040]
+    end_coord = [54340.7421875, 74262.7265625, 1041]
+    layer_name = TEST_WRITE_FILE
+    resolution = [4, 4, 30]
+
+    bcubes_or_points = [
+        BoundingCube.from_coords(start_coord, end_coord, resolution),
+        [181038.53125, 283613.8125, 28860.00183105],
+    ]
+
+    ngl_state.write_remote_annotations(layer_name, resolution, bcubes_or_points)
+    cf = CloudFiles(REMOTE_LAYERS_PATH)
+    layer_json = cf.get_json(TEST_WRITE_FILE)
+    assert len(layer_json["annotations"]) == 2
---
+++ zetta_utils/parsing/ngl_state.py
@@ -0,0 +1,118 @@
+"""neuroglancer state parsing."""
+
+from enum import Enum
+from os import environ
+from typing import Dict, List, Union
+
+import numpy as np
+from cloudfiles import CloudFiles
+from neuroglancer.viewer_state import (
+    AnnotationLayer,
+    AxisAlignedBoundingBoxAnnotation,
+    PointAnnotation,
+    make_layer,
+)
+
+from zetta_utils.bcube import BoundingCube
+from zetta_utils.bcube.bcube import BoundingBoxND
+from zetta_utils.log import get_logger
+from zetta_utils.typing import Vec3D
+
+logger = get_logger("zetta_utils")
+
+
+class NglLayerKeys(Enum):
+    ANNOTATION_COLOR = "annotationColor"
+    ANNOTATIONS = "annotations"
+    NAME = "name"
+    RESOLUTION = "voxelSize"
+    TOOL = "tool"
+    TYPE = "type"
+
+
+class AnnotationKeys(Enum):
+    ID = "id"
+    POINT = "point"
+    POINT_A = "pointA"
+    POINT_B = "pointB"
+    TYPE = "type"
+
+
+class DefaultLayerValues(Enum):
+    COLOR = "#ff0000"
+    TYPE = "annotation"
+    TOOL = "annotateBoundingBox"
+
+
+def read_remote_annotations(layer_name: str) -> List[Union[BoundingCube, Vec3D]]:
+    remote_path = environ.get("REMOTE_LAYERS_PATH", "gs://remote-annotations")
+    logger.info(f"Remote layer: {remote_path}/{layer_name}.")
+
+    cf = CloudFiles(remote_path)
+    layer_json = cf.get_json(layer_name)
+    logger.debug(layer_json)
+    layer: AnnotationLayer = make_layer(layer_json)
+
+    logger.info(f"Layer type: {layer.type}; Total: {len(layer.annotations)}.")
+    return _parse_annotations(layer)
+
+
+def _parse_annotations(layer: AnnotationLayer) -> List[Union[BoundingCube, Vec3D]]:
+    result: List[Union[BoundingCube, Vec3D]] = []
+    resolution: Vec3D = layer.to_json()[NglLayerKeys.RESOLUTION.value]
+    for annotation in layer.annotations:
+        assert isinstance(
+            annotation, (AxisAlignedBoundingBoxAnnotation, PointAnnotation)
+        ), "Annotation type not supported."
+        try:
+            bcube = BoundingCube.from_coords(
+                annotation.point_a,
+                annotation.point_b,
+                resolution=resolution,
+            )
+            result.append(bcube)
+        except AttributeError:
+            point: Vec3D = annotation.point * resolution
+            result.append(point)
+    return result
+
+
+def write_remote_annotations(
+    layer_name: str,
+    resolution: Vec3D,
+    bcubes_or_points: List[Union[BoundingCube, Vec3D]],
+) -> None:
+    remote_path = environ.get("REMOTE_LAYERS_PATH", "gs://remote-annotations")
+    annotations: List[Dict] = []
+    layer_d = {
+        NglLayerKeys.NAME.value: layer_name,
+        NglLayerKeys.RESOLUTION.value: resolution,
+        NglLayerKeys.TOOL.value: DefaultLayerValues.TOOL.value,
+        NglLayerKeys.TYPE.value: DefaultLayerValues.TYPE.value,
+        NglLayerKeys.ANNOTATION_COLOR.value: DefaultLayerValues.COLOR.value,
+        NglLayerKeys.ANNOTATIONS.value: annotations,
+    }
+
+    for i, bcubes_or_point in enumerate(bcubes_or_points):
+        if isinstance(bcubes_or_point, BoundingBoxND):
+            x, y, z = bcubes_or_point.bounds
+            point_a = np.array([x[0], y[0], z[0]])
+            point_b = np.array([x[1], y[1], z[1]])
+            annotation = {
+                AnnotationKeys.ID.value: str(i),
+                AnnotationKeys.POINT_A.value: point_a / resolution,
+                AnnotationKeys.POINT_B.value: point_b / resolution,
+                AnnotationKeys.TYPE.value: AxisAlignedBoundingBoxAnnotation().type,
+            }
+            annotations.append(annotation)
+        else:
+            annotation = {
+                AnnotationKeys.ID.value: str(i),
+                AnnotationKeys.POINT.value: np.array(bcubes_or_point) / resolution,
+                AnnotationKeys.TYPE.value: PointAnnotation().type,
+            }
+            annotations.append(annotation)
+
+    cf = CloudFiles(remote_path)
+    logger.info(f"Writing {len(bcubes_or_points)} bcubes/points to {remote_path}/{layer_name}.")
+    cf.put_json(layer_name, make_layer(layer_d).to_json())

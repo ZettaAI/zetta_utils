2b5b2e09fa31ad05e5fd0132e84ac64685a8f34c
Dodam Ih <dodam@zetta.ai>
2022-12-05 01:51:47 -0800

    high res encoding coarsener cue and regime, cue using new sequential rather than artificery

--- zetta_utils/training/lightning/regimes/__init__.py
+++ zetta_utils/training/lightning/regimes/__init__.py
@@ -1,3 +1,4 @@
 from . import naive_supervised
 from .encoding_coarsener import EncodingCoarsener
+from .encoding_coarsener_highres import EncodingCoarsenerHighRes
 from .naive_supervised import NaiveSupervised

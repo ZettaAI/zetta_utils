248b788603db9c080f05a1266ba4cae83f87cbbe
Dodam Ih <dodam@zetta.ai>
2022-12-05 00:38:39 -0800

    misalignment detector regime commitZ

--- zetta_utils/training/lightning/regimes/__init__.py
+++ zetta_utils/training/lightning/regimes/__init__.py
@@ -1,3 +1,4 @@
 from . import naive_supervised
 from .encoding_coarsener import EncodingCoarsener
 from .naive_supervised import NaiveSupervised
+from .misalignment_detector import MisalignmentDetector

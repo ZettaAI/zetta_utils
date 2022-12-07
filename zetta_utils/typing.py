20b6ba31dc2dcd106b2f6e9ee206a8d86943d7da
Dodam Ih <dodam@zetta.ai>
2022-12-07 02:44:32 -0800

    initial commit for Vec3D update

--- zetta_utils/typing.py
+++ zetta_utils/typing.py
@@ -1,13 +1,38 @@
 """Basic type definitions used for type annotations."""
 from __future__ import annotations
 
-from typing import Any, List, Tuple, Union
+from typing import Any, Tuple
 
+import numpy as np
 import typeguard
 
 Slices3D = Tuple[slice, slice, slice]
-Vec3D = Union[Tuple[float, float, float], Tuple[int, int, int], List[float], List[int]]
-IntVec3D = Union[Tuple[int, int, int], List[int]]
+
+
+class Vec3D(np.ndarray):  # pragma: no cover
+    def __new__(cls, vec: Tuple[float, float, float]):
+        # check necessary for runtime
+        assert len(vec) == 3, "Vec3D must have 3 elements"
+        return np.asarray(vec, dtype=np.float64).view(cls)
+
+    # avoid returning Vec3D whenever sliced, defaulting to float or tuple
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.view(np.ndarray).__getitem__(key)
+        return tuple(self.view(np.ndarray).__getitem__(key))
+
+
+class IntVec3D(np.ndarray):  # pragma: no cover
+    def __new__(cls, vec: Tuple[int, int, int]):
+        # check necessary for runtime
+        assert len(vec) == 3, "IntVec3D must have 3 elements"
+        return np.asarray(vec, dtype=np.int64).view(cls)
+
+    # avoid returning IntVec3D whenever sliced, defaulting to int or tuple
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.view(np.ndarray).__getitem__(key)
+        return tuple(self.view(np.ndarray).__getitem__(key))
 
 
 def check_type(obj: Any, cls: Any) -> bool:  # pragma: no cover

e56199486c4641fc4a865fa980e793b2dc737900
Akhilesh Halageri <akhilesh@zetta.ai>
2022-12-10 18:26:42 +0000

    feat: write single key value

--- zetta_utils/layer/kv_store/datastore/backend.py
+++ zetta_utils/layer/kv_store/datastore/backend.py
@@ -1,18 +1,40 @@
 """gcloud datastore backend"""
 
-from typing import Any, TypeVar
+from typing import List, Mapping, TypeVar, Union
 
 import attrs
+from google.cloud.datastore import Client, Entity
 from typeguard import typechecked
 
 from zetta_utils import builder
-from zetta_utils.layer import LayerBackend, LayerIndex
+from zetta_utils.layer import LayerBackend
 
-IndexT = TypeVar("IndexT", bound=LayerIndex)
+from .index import DataStoreIndex
+
+DataT = TypeVar("DataT", bound=str)
+DataAndAttributesT = Mapping[str, str]
+ValueT = Union[DataT, DataAndAttributesT]
+MultiValueT = Union[List[DataT], List[DataAndAttributesT]]
 
 
 @builder.register("DatastoreBackend")
 @typechecked
 @attrs.mutable
-class DatastoreBackend(LayerBackend[IndexT, Any]):
-    ...
+class DatastoreBackend(LayerBackend[DataStoreIndex, Union[ValueT, MultiValueT]]):
+    client: Client = Client()
+
+    def read(self, idx: DataStoreIndex) -> Union[ValueT, MultiValueT]:
+        ...
+
+    def write(self, idx: DataStoreIndex, value: Union[ValueT, MultiValueT]):
+        # scalar key value
+        if not isinstance(value, list):
+            assert len(idx.keys) == 1, f"Number of keys {len(idx.keys)} but value is single."
+            key = idx.keys[0]
+            entity = Entity(key)
+            value = value if isinstance(value, Mapping) else {"value": value}
+            entity.update(value)
+            self.client.put(entity)
+
+    def get_name(self) -> str:  # pragma: no cover
+        return self.client.base_url

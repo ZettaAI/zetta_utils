b4e7bc858323d734139850086399beb0db98e349
Akhilesh Halageri <akhilesh@zetta.ai>
2022-12-10 15:11:28 +0000

    fix: cache keys

--- zetta_utils/layer/kv_store/datastore/index.py
+++ zetta_utils/layer/kv_store/datastore/index.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-docstring
 from __future__ import annotations
 
-from typing import Iterable, Optional, Tuple
+from typing import List, Optional, Tuple
 
 import attrs
 from google.cloud.datastore import Key
@@ -20,22 +20,29 @@ class DataStoreIndex(KVIndex):
     idx_raw: ConvertibleKVIndex
     attributes: Optional[Tuple[str]] = None
 
+    _keys: Optional[List[Key]] = None
+
     @classmethod
     def default_convert(cls, idx_raw: RawKVIndex) -> DataStoreIndex:
         raise NotImplementedError()
 
-    def _key(self, raw_key: str) -> Key:
+    def _make_key(self, raw_key: str) -> Key:
         return Key(self.kind, raw_key, project=self.project, namespace=self.namespace)
 
-    def keys(self) -> Iterable[Key]:
+    def keys(self) -> List[Key]:
+
+        if self._keys is not None:
+            return self._keys
 
         # simple string index
         if isinstance(self.idx_raw, str):
-            return [self._key(self.idx_raw)]
+            self._keys = [self._make_key(self.idx_raw)]
+            return self._keys
 
         # multi-valued index, without attributes
         if isinstance(self.idx_raw, list):
-            return [self._key(i) for i in self.idx_raw]
+            self._keys = [self._make_key(i) for i in self.idx_raw]
+            return self._keys
 
         # index, with attributes
         if isinstance(self.idx_raw, tuple):

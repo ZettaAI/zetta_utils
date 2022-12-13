4f1d39437b0017701d14f42ee9290b49dadf497a
Sergiy Popovych <sergiy.popovich@gmail.com>
2022-12-13 18:06:04 +0000

    feat: `concurrent_flow`

--- zetta_utils/mazepa/__init__.py
+++ zetta_utils/mazepa/__init__.py
@@ -5,7 +5,16 @@ from . import exceptions
 from .task_outcome import TaskOutcome, TaskStatus
 
 from .tasks import Task, TaskableOperation, taskable_operation, taskable_operation_cls
-from .flows import Flow, FlowFnReturnType, FlowSchema, flow_schema, flow_schema_cls, Dependency
+from .flows import (
+    Flow,
+    FlowFnReturnType,
+    FlowSchema,
+    flow_schema,
+    flow_schema_cls,
+    Dependency,
+    seq_flow,
+    concurrent_flow,
+)
 
 from .execution_queue import ExecutionMultiQueue, ExecutionQueue, LocalExecutionQueue
 from .execution_state import ExecutionState, InMemoryExecutionState
@@ -14,4 +23,4 @@ from .remote_execution_queues import SQSExecutionQueue
 from .execute import Executor, execute
 from .worker import run_worker
 
-from .tools import SubflowTask, seq_flow
+from .tools import SubflowTask

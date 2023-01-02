90ec4ff443ae06ffcfe546041c33a36ad631f272
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-01-02 21:25:32 +0000

    feat: affine transfrom, 3d tile aug

---
+++ tests/unit/tensor_ops/test_transfrom.py
@@ -0,0 +1,89 @@
+# pylint: disable=missing-docstring,invalid-name
+import pytest
+import torch
+
+from zetta_utils.tensor_ops import transform
+
+
+@pytest.mark.parametrize(
+    "data, kwargs, expected",
+    [
+        [
+            torch.tensor(
+                [
+                    [1, 1],
+                    [1, 1],
+                ],
+                dtype=torch.float,
+            ),
+            {"trans_x_px": 1},
+            torch.tensor(
+                [
+                    [0, 1],
+                    [0, 1],
+                ],
+                dtype=torch.float,
+            ),
+        ],
+        [
+            torch.tensor(
+                [
+                    [1, 1],
+                    [1, 1],
+                ],
+                dtype=torch.float,
+            ),
+            {
+                "trans_x_px": 1,
+                "trans_y_px": 1,
+            },
+            torch.tensor(
+                [
+                    [0, 0],
+                    [0, 1],
+                ],
+                dtype=torch.float,
+            ),
+        ],
+        [
+            torch.tensor(
+                [
+                    [1, 1],
+                    [1, 1],
+                ],
+                dtype=torch.float,
+            ),
+            {"trans_x_px": 1, "trans_y_px": 1, "rot_deg": 90},
+            torch.tensor(
+                [
+                    [0, 0],
+                    [1, 0],
+                ],
+                dtype=torch.float,
+            ),
+        ],
+        [
+            torch.tensor(
+                [
+                    [1, 1],
+                    [1, 1],
+                ],
+                dtype=torch.float,
+            ),
+            {
+                "shear_x_deg": 45,
+            },
+            torch.tensor(
+                [
+                    [1, 0.5],
+                    [0.5, 1],
+                ],
+                dtype=torch.float,
+            ),
+        ],
+    ],
+)
+def test_get_affine_field(data, kwargs, expected):
+    field = transform.get_affine_field(size=data.shape[-1], **kwargs)
+    result = field.from_pixels().sample(data)
+    torch.testing.assert_close(result, expected)
--- zetta_utils/tensor_ops/transform.py
+++ zetta_utils/tensor_ops/transform.py
@@ -5,18 +5,31 @@ import torchfields  # pylint: disable=unused-import
 
 def get_affine_field(
     size,
-    rot_deg=0,
-    scale=1,
-    shear_x_px=0,
-    shear_y_px=0,
     trans_x_px=0,
     trans_y_px=0,
+    rot_deg=0,
+    shear_x_deg=0,
+    shear_y_deg=0,
+    scale=1,
 ):
+    """
+    Return 2D displacement field that represents the given affine transformation.
+    Transformations are applied in the following order -- translation->rotation->shear->scale.
+    Note that the resulting field is represented in pixel magnitudes.
+
+    :param size: Shape along the X and Y dimension of the resulting field.
+    :param trans_x_px: X translation in pixels, from left to right.
+    :param trans_y_px: Y translation in pixels, from top to bottom.
+    :param rot_deg: Rotation degrees, clockwise
+    :param shear_x_deg: X shear degrees.
+    :param shear_y_deg: Y shear degrees.
+
+    """
     aff = (
-        affine.Affine.translation(trans_x_px * 2 / size, trans_y_px * 2 / size)
-        * affine.Affine.rotation(rot_deg)
-        * affine.Affine.shear(shear_x_px, shear_y_px)
-        * affine.Affine.scale(scale)
+        affine.Affine.translation(-trans_x_px * 2 / size, -trans_y_px * 2 / size)
+        * affine.Affine.rotation(-rot_deg)
+        * affine.Affine.shear(-shear_x_deg, -shear_y_deg)
+        * affine.Affine.scale(1 / scale)
     )
     mat = torch.tensor([[aff.a, aff.b, aff.c], [aff.d, aff.e, aff.f]]).unsqueeze(0)
     field = torch.Field.affine_field(mat, size=(1, 2, size, size))

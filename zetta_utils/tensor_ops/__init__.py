99806a15f4cbd7bcd42431d3128ee4a5e90eb795
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-01-02 20:23:04 +0000

    fix: preserve `@skip_on_empty` typing

--- zetta_utils/tensor_ops/__init__.py
+++ zetta_utils/tensor_ops/__init__.py
@@ -1,6 +1,6 @@
 from . import traceback_supress
 
-from . import common, convert, mask
+from . import common, convert, mask, transform
 from .common import InterpolationMode, compare, crop, interpolate, squeeze, unsqueeze
 from .convert import astype, to_np, to_torch
 from .mask import filter_cc  # , coarsen

2aa5c05d99ed57eec2ecd4f1c8b7b16052c4c405
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-01-03 03:40:38 +0000

    chore: format fixup

--- zetta_utils/tensor_ops/__init__.py
+++ zetta_utils/tensor_ops/__init__.py
@@ -1,6 +1,6 @@
 from . import traceback_supress
 
 from . import common, convert, mask, transform
-from .common import InterpolationMode, compare, crop, interpolate, squeeze, unsqueeze
+from .common import InterpolationMode, compare, crop, interpolate, squeeze, unsqueeze, unsqueeze_to
 from .convert import astype, to_np, to_torch
 from .mask import filter_cc  # , coarsen

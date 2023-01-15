c6b3073f8e71e35852518a48ec03ea413ca66f97
Akhilesh Halageri <akhilesh@zetta.ai>
2023-01-15 14:59:33 -0600

    generic db layer, datastore backend (#157)

---
+++ zetta_utils/layer/db_layer/__init__.py
@@ -0,0 +1,6 @@
+from .index import (
+    DBIndex,
+)
+from .frontend import DBFrontend, DataT, UserDBIndex, ValueT, MultiValueT, RowDataT, ColIndex
+
+from .build import build_db_layer, DBLayer
---
+++ zetta_utils/layer/db_layer/index.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-docstring
+from __future__ import annotations
+
+from typing import List, Mapping, Tuple
+
+import attrs
+
+from zetta_utils import builder
+
+
+@builder.register("DBIndex")
+@attrs.frozen
+class DBIndex:
+    row_col_keys: Mapping[str, Tuple[str, ...]]
+
+    @property
+    def row_keys(self) -> List[str]:
+        return list(self.row_col_keys.keys())
+
+    @property
+    def col_keys(self) -> List[Tuple[str, ...]]:
+        return list(self.row_col_keys.values())
+
+    def get_size(self):  # pragma: no cover
+        return len(self.row_col_keys)

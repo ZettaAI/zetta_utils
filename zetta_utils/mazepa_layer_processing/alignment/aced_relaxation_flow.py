291d21508bf2d40833fe3c2f4890cfcb5dd89546
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-01-26 18:02:53 +0000

    feat: error correcting ACED

--- zetta_utils/mazepa_layer_processing/alignment/aced_relaxation_flow.py
+++ zetta_utils/mazepa_layer_processing/alignment/aced_relaxation_flow.py
@@ -13,14 +13,43 @@ from zetta_utils.layer.volumetric import (
 from ..common import build_chunked_volumetric_callable_flow_schema
 
 
+@builder.register("build_get_match_offsets_flow")
+def build_get_match_offsets_flow(
+    chunk_size: IntVec3D,
+    bbox: BBox3D,
+    dst_resolution: Vec3D,
+    non_tissue: VolumetricLayer,
+    dst: VolumetricLayer,
+    misd_mask_zm1: VolumetricLayer,
+    misd_mask_zm2: Optional[VolumetricLayer] = None,
+    misd_mask_zm3: Optional[VolumetricLayer] = None,
+) -> mazepa.Flow:
+    flow_schema = build_chunked_volumetric_callable_flow_schema(
+        fn=alignment.aced_relaxation.get_aced_match_offsets,
+        chunker=VolumetricIndexChunker(chunk_size=chunk_size),
+    )
+    flow = flow_schema(
+        idx=VolumetricIndex(bbox=bbox, resolution=dst_resolution),
+        non_tissue=non_tissue,
+        dst=dst,
+        misd_mask_zm1=misd_mask_zm1,
+        misd_mask_zm2=misd_mask_zm2,
+        misd_mask_zm3=misd_mask_zm3,
+    )
+    return flow
+
+
 @builder.register("build_aced_relaxation_flow")
 def build_aced_relaxation_flow(
     chunk_size: IntVec3D,
     bbox: BBox3D,
     dst_resolution: Vec3D,
     dst: VolumetricLayer,
-    field: VolumetricLayer,
+    match_offsets: VolumetricLayer,
+    field_zm1: VolumetricLayer,
     crop_pad: IntVec3D,
+    field_zm2: Optional[VolumetricLayer] = None,
+    field_zm3: Optional[VolumetricLayer] = None,
     num_iter: int = 100,
     lr: float = 0.3,
     rigidity_weight: float = 10.0,
@@ -31,10 +60,14 @@ def build_aced_relaxation_flow(
         chunker=VolumetricIndexChunker(chunk_size=chunk_size),
         crop_pad=crop_pad,
     )
+
     flow = flow_schema(
         idx=VolumetricIndex(bbox=bbox, resolution=dst_resolution),
         dst=dst,
-        field=field,
+        match_offsets=match_offsets,
+        field_zm1=field_zm1,
+        field_zm2=field_zm2,
+        field_zm3=field_zm3,
         num_iter=num_iter,
         lr=lr,
         rigidity_weight=rigidity_weight,

db4d288b402c548c79ac0969b58ee467e18f377e
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-01-26 18:02:53 +0000

    feat: error correcting ACED

--- zetta_utils/alignment/aced_relaxation.py
+++ zetta_utils/alignment/aced_relaxation.py
@@ -1,3 +1,6 @@
+# pylint: disable=too-many-locals
+from __future__ import annotations
+
 from typing import Dict, List, Literal, Optional, Tuple
 
 import einops
@@ -11,46 +14,83 @@ logger = log.get_logger("zetta_utils")
 
 
 def compute_aced_loss(
-    pfields: Dict[Tuple[int, int], torch.Tensor], afields: List[torch.Tensor], rigidity_weight
+    pfields: Dict[Tuple[int, int], torch.Tensor],
+    afields: List[torch.Tensor],
+    match_offsets: List[torch.Tensor],
+    rigidity_weight: float,
 ) -> torch.Tensor:
-    inter_loss = 0
     intra_loss = 0
+    inter_loss = 0
+
     for i in range(1, len(afields)):
-        inter_loss_map = (
-            pfields[(i, i - 1)]
-            .from_pixels()(  # type: ignore
-                afields[i - 1].from_pixels()  # type: ignore
+        offset = 1
+        while (i, i - offset) in pfields:
+            inter_loss_map = (
+                pfields[(i, i - offset)]
+                .from_pixels()(  # type: ignore
+                    afields[i - offset].from_pixels()  # type: ignore
+                )
+                .pixels()
+                - afields[i]
             )
-            .pixels()
-            - afields[i]
-        )
+            inter_loss_map_mask = (
+                afields[i]
+                .from_pixels()((match_offsets[i] == offset).float())  # type: ignore
+                .squeeze()
+                > 0.0
+            )
+            this_inter_loss = (inter_loss_map[..., inter_loss_map_mask] ** 2).sum()
+            # this_inter_loss = (inter_loss_map ** 2).mean()
 
-        intra_loss_map = metroem.loss.rigidity(afields[i])
-        this_inter_loss = (inter_loss_map ** 2).mean()
-        this_intra_loss = intra_loss_map.mean()
+            intra_loss_map = metroem.loss.rigidity(afields[i])
+            this_intra_loss = intra_loss_map.sum()
 
-        intra_loss += this_intra_loss
-        inter_loss += this_inter_loss
+            intra_loss += this_intra_loss
+            inter_loss += this_inter_loss
+            offset += 1
 
     loss = inter_loss + rigidity_weight * intra_loss
     print(inter_loss, rigidity_weight * intra_loss, loss)
-    return loss
+    return loss  # type: ignore
 
 
 @builder.register("perform_aced_relaxation")
 def perform_aced_relaxation(
-    field: torch.Tensor,
+    match_offsets: torch.Tensor,
+    field_zm1: torch.Tensor,
+    field_zm2: Optional[torch.Tensor] = None,
+    field_zm3: Optional[torch.Tensor] = None,
     num_iter=100,
     lr=0.3,
     rigidity_weight=10.0,
     fix: Optional[Literal["first", "last", "both"]] = "first",
 ) -> torch.Tensor:
-    field_xy = einops.rearrange(field, "C X Y Z -> Z C X Y").field()  # type: ignore
-    num_sections = field_xy.shape[0]
-    afields = [torch.zeros_like(field_xy[0:1]).cuda() for _ in range(num_sections)]
+    if (match_offsets != 0).sum() == 0:
+        return torch.zeros_like(field_zm1)
 
-    for i in range(num_sections):
-        afields[i].requires_grad = True
+    match_offsets_zcxy = einops.rearrange(match_offsets, "C X Y Z -> Z C X Y").cuda()
+    num_sections = match_offsets_zcxy.shape[0]
+    assert num_sections > 1, "Can't relax blocks with just one section"
+    field_map = {
+        1: field_zm1,
+        2: field_zm2,
+        3: field_zm3,
+    }
+
+    pfields: Dict[Tuple[int, int], torch.Tensor] = {}
+    for offset, field in field_map.items():
+        if field is not None:
+            field_zcxy = einops.rearrange(field, "C X Y Z -> Z C X Y").field()  # type: ignore
+
+            for i in range(num_sections):
+                pfields[(i, i - offset)] = field_zcxy[i : i + 1].cuda()
+
+    afields = [
+        torch.zeros((1, 2, match_offsets_zcxy.shape[2], match_offsets_zcxy.shape[3]))
+        .cuda()
+        .field()  # type: ignore
+        for _ in range(num_sections)
+    ]
 
     if fix is None:
         opt_range = range(num_sections)
@@ -62,14 +102,21 @@ def perform_aced_relaxation(
         assert fix == "both"
         opt_range = range(1, num_sections - 1)
 
+    for i in opt_range:
+        afields[i].requires_grad = True
+
     optimizer = torch.optim.Adam(
         [afields[i] for i in opt_range],
         lr=lr,
     )
 
-    pfields = {(i, i - 1): field_xy[i : i + 1].cuda() for i in range(num_sections)}
     for i in range(num_iter):
-        loss = compute_aced_loss(pfields=pfields, afields=afields, rigidity_weight=rigidity_weight)
+        loss = compute_aced_loss(
+            pfields=pfields,
+            afields=afields,
+            match_offsets=[match_offsets_zcxy[i] for i in range(num_sections)],
+            rigidity_weight=rigidity_weight,
+        )
         if loss < 0.005:
             break
         print(loss)
@@ -79,4 +126,33 @@ def perform_aced_relaxation(
 
     result_xy = torch.cat(afields, 0)
     result = einops.rearrange(result_xy, "Z C X Y -> C X Y Z")
+    return result
+
+
+def get_aced_match_offsets(
+    non_tissue: torch.Tensor,
+    misd_mask_zm1: torch.Tensor,
+    misd_mask_zm2: Optional[torch.Tensor] = None,
+    misd_mask_zm3: Optional[torch.Tensor] = None,
+) -> torch.Tensor:
+
+    match_offsets = torch.ones_like(non_tissue, dtype=torch.int) * -1
+    match_offsets[non_tissue] = 0
+
+    misd_mask_map = {
+        1: misd_mask_zm1,
+        2: misd_mask_zm2,
+        3: misd_mask_zm3,
+    }
+
+    for offset in sorted(misd_mask_map.keys()):
+        unmatched_locations = match_offsets == -1
+        if unmatched_locations.sum() == 0:
+            break
+        if misd_mask_map[offset] is not None:
+            current_match_locations = misd_mask_map[offset] == 0
+            match_offsets[unmatched_locations * current_match_locations] = offset
+
+    match_offsets[match_offsets == -1] = 0
+    result = match_offsets.byte()
     return result
--- zetta_utils/alignment/misalignment_detector.py
+++ zetta_utils/alignment/misalignment_detector.py
@@ -3,7 +3,7 @@ import einops
 import torch
 from typeguard import typechecked
 
-from zetta_utils import builder, convnet
+from zetta_utils import builder, convnet, tensor_ops
 
 
 @builder.register("MisalignmentDetector")
@@ -30,19 +30,32 @@ class MisalignmentDetector:
             # the input is assumed to be uint8 encoding
             assert src.dtype == torch.uint8
             assert tgt.dtype == torch.uint8
+            src_zcxy = einops.rearrange(src, "C X Y Z -> Z C X Y").float()
+            tgt_zcxy = einops.rearrange(tgt, "C X Y Z -> Z C X Y").float()
 
-            data_in = (
-                einops.rearrange(torch.cat((src, tgt), 0), "C X Y Z -> Z C X Y").float() - 127.0
-            )
+            data_in = torch.cat((src_zcxy, tgt_zcxy), 1) - 127.0
             data_in /= 255.0
 
             result = model(data_in.to(device))
+
             result = einops.rearrange(result, "Z C X Y -> C X Y Z")
+            src_mask = tensor_ops.mask.filter_cc(
+                (src == 127) + (src == 0),
+                mode="keep_large",
+                thr=1000,
+            )
+            tgt_mask = tensor_ops.mask.filter_cc(
+                (tgt == 127) + (tgt == 0),
+                mode="keep_large",
+                thr=1000,
+            )
+            result[src_mask] = 1
+            result[tgt_mask] = 1
+
             assert result.shape[0] == 1
 
             assert result.max() <= 1, "Final layer of misalignment detector assumed to be sigmoid"
             assert result.max() >= 0, "Final layer of misalignment detector assumed to be sigmoid"
-
             result = 255.0 * result
 
         return result.byte().to(src.device)

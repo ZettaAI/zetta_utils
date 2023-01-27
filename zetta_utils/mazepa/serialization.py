7919077cdee6e244562ee7b78a7a9b97dc309399
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-01-27 17:22:11 +0000

    feat: pickle not dill

--- zetta_utils/mazepa/serialization.py
+++ zetta_utils/mazepa/serialization.py
@@ -1,27 +1,34 @@
 from __future__ import annotations
 
 import codecs
+import pickle
 import zlib
-
-import dill
 
 from .exceptions import MazepaException
 
 
 def serialize(obj):  # pragma: no cover
-    return codecs.encode(zlib.compress(dill.dumps(obj, protocol=4)), "base64").decode()
+    result = codecs.encode(zlib.compress(pickle.dumps(obj, protocol=4)), "base64").decode()
+    return result
+
+
+def test(obj):  # pragma: no cover
+    ser = serialize(obj)
+    deser = deserialize(ser)
+    return deser == obj
 
 
 def deserialize(s):  # pragma: no cover
     try:
-        result = dill.loads(zlib.decompress(codecs.decode(s.encode(), "base64")))
+        result = pickle.loads(zlib.decompress(codecs.decode(s.encode(), "base64")))
     except MazepaException as e:
         raise e
     except Exception as e:
-        raise RuntimeError(
-            "Encountered an error during desearilization, indicating a mismatch "
+        e.args = (
+            f"{e}\n This exception occured during desearilization, indicating a mismatch "
             "between serialization and deserialization environments. "
             "This is most likely caused by worker image being _not_ up to date, "
-            "or scheduler and worker are running different python versions."
-        ) from e
+            "or scheduler and worker are running different python versions.",
+        )
+        raise e
     return result

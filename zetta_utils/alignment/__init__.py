0b18142f350f29a174331268fa19ee4b64b5a094
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-01-28 17:31:45 +0000

    feat: coarsener inference

--- zetta_utils/alignment/__init__.py
+++ zetta_utils/alignment/__init__.py
@@ -2,4 +2,5 @@ from . import field_profilers
 from . import online_finetuner
 from . import aced_relaxation
 from .base_encoder import BaseEncoder
+from .encoding_coarsener import EncodingCoarsener
 from .misalignment_detector import MisalignmentDetector

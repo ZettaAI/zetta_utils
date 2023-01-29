bfc2a7b42f0f876f6aed785eac99d0efc94da24c
Akhilesh Halageri <akhilesh@zetta.ai>
2023-01-29 18:48:28 +0000

    fix: convert dict to k8s objects

--- zetta_utils/mazepa_addons/resource_allocation/gcp_k8s.py
+++ zetta_utils/mazepa_addons/resource_allocation/gcp_k8s.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import json
 import os
 from contextlib import ExitStack, contextmanager
-from typing import Any, Dict, Iterable, Optional, Union
+from typing import Any, Dict, Iterable, Optional, Union, no_type_check
 
 import kubernetes as k8s
 from zetta_utils import builder, log, mazepa
@@ -30,6 +30,38 @@ def get_worker_command(queue_spec: Dict[str, Any]):
     return result
 
 
+def _get_worker_env_vars(env_secret_mapping: Dict[str, str]) -> list:
+    name_path_map = {
+        "MY_NODE_NAME": "spec.nodeName",
+        "MY_POD_NAME": "metadata.name",
+        "MY_POD_NAMESPACE": "metadata.namespace",
+        "MY_POD_IP": "status.podIP",
+        "MY_POD_SERVICE_ACCOUNT": "spec.serviceAccountName",
+    }
+    envs = [
+        k8s.client.V1EnvVar(  # type: ignore
+            name=name,
+            value_from=k8s.client.V1EnvVarSource(  # type: ignore
+                field_ref=k8s.client.V1ObjectFieldSelector(field_path=path)  # type: ignore
+            ),
+        )
+        for name, path in name_path_map.items()
+    ]
+
+    for k, v in env_secret_mapping.items():
+        env_var = k8s.client.V1EnvVar(  # type: ignore
+            name=k,
+            value_from=k8s.client.V1EnvVarSource(  # type: ignore
+                secret_key_ref=k8s.client.V1SecretKeySelector(  # type: ignore
+                    key="value", name=v, optional=False
+                )
+            ),
+        )
+        envs.append(env_var)
+    return envs
+
+
+@no_type_check
 def get_worker_deployment_spec(
     name: str,
     image: str,
@@ -38,119 +70,82 @@ def get_worker_deployment_spec(
     resources: Dict[str, int | float | str],
     labels: Dict[str, str],
     env_secret_mapping: Dict[str, str],
-) -> Dict[str, Any]:
-    result: Dict[str, Any] = {
-        "apiVersion": "apps/v1",
-        "kind": "Deployment",
-        "metadata": {
-            "labels": labels,
-            "name": name,
-            "namespace": "default",
-        },
-        "spec": {
-            "progressDeadlineSeconds": 600,
-            "replicas": replicas,
-            "revisionHistoryLimit": 10,
-            "selector": {
-                "matchLabels": labels,
-            },
-            "strategy": {
-                "rollingUpdate": {"maxSurge": "25%", "maxUnavailable": "25%"},
-                "type": "RollingUpdate",
-            },
-            "template": {
-                "metadata": {
-                    "creationTimestamp": None,
-                    "labels": labels,
-                },
-                "spec": {
-                    "containers": [
-                        {
-                            "name": "zutils-worker",
-                            "env": [
-                                {
-                                    "name": "MY_NODE_NAME",
-                                    "valueFrom": {"fieldRef": {"fieldPath": "spec.nodeName"}},
-                                },
-                                {
-                                    "name": "MY_POD_NAME",
-                                    "valueFrom": {"fieldRef": {"fieldPath": "metadata.name"}},
-                                },
-                                {
-                                    "name": "MY_POD_NAMESPACE",
-                                    "valueFrom": {"fieldRef": {"fieldPath": "metadata.namespace"}},
-                                },
-                                {
-                                    "name": "MY_POD_IP",
-                                    "valueFrom": {"fieldRef": {"fieldPath": "status.podIP"}},
-                                },
-                                {
-                                    "name": "MY_POD_SERVICE_ACCOUNT",
-                                    "valueFrom": {
-                                        "fieldRef": {"fieldPath": "spec.serviceAccountName"}
-                                    },
-                                },
-                            ],
-                            "args": ["-c", worker_command],
-                            "command": ["/bin/sh"],
-                            "image": image,
-                            "imagePullPolicy": "IfNotPresent",
-                            "resources": {
-                                "limits": resources,
-                                "requests": resources,
-                            },
-                            "terminationMessagePath": "/dev/termination-log",
-                            "terminationMessagePolicy": "File",
-                            "volumeMounts": [
-                                {
-                                    "mountPath": "/root/.cloudvolume/secrets",
-                                    "name": "cloudvolume-secrets",
-                                    "readOnly": True,
-                                },
-                                {"mountPath": "/tmp", "name": "tmp"},
-                                {"mountPath": "/dev/shm", "name": "dshm"},
-                            ],
-                        }
-                    ],
-                    "tolerations": [
-                        {
-                            "key": "worker-pool",
-                            "operator": "Equal",
-                            "value": "true",
-                            "effect": "NoSchedule",
-                        }
-                    ],
-                    "dnsPolicy": "Default",
-                    "restartPolicy": "Always",
-                    "schedulerName": "default-scheduler",
-                    "securityContext": {},
-                    "terminationGracePeriodSeconds": 30,
-                    "volumes": [
-                        {
-                            "name": "cloudvolume-secrets",
-                            "secret": {"defaultMode": 420, "secretName": "cloudvolume-secrets"},
-                        },
-                        {"emptyDir": {"medium": "Memory"}, "name": "tmp"},
-                        {"emptyDir": {"medium": "Memory"}, "name": "dshm"},
-                    ],
-                },
-            },
-        },
-    }
-    for k, v in env_secret_mapping.items():
-        result["spec"]["template"]["spec"]["containers"][0]["env"].append(
-            {
-                "name": k,
-                "valueFrom": {
-                    "secretKeyRef": {
-                        "name": v,
-                        "key": "value",
-                        "optional": False,
-                    }
-                },
-            }
-        )
-    return result
+) -> k8s.client.V1Deployment:
+    volume_mounts = [
+        k8s.client.V1VolumeMount(
+            mount_path="/root/.cloudvolume/secrets", name="cloudvolume-secrets", read_only=True
+        ),
+        k8s.client.V1VolumeMount(mount_path="/dev/shm", name="dshm"),
+        k8s.client.V1VolumeMount(mount_path="/tmp", name="tmp"),
+    ]
+
+    container = k8s.client.V1Container(
+        args=["-c", worker_command],
+        command=["/bin/sh"],
+        env=_get_worker_env_vars(env_secret_mapping),
+        name="zutils-worker",
+        image=image,
+        image_pull_policy="IfNotPresent",
+        resources=k8s.client.V1ResourceRequirements(
+            requests=resources,
+            limits=resources,
+        ),
+        termination_message_path="/dev/termination-log",
+        termination_message_policy="File",
+        volume_mounts=volume_mounts,
+    )
+
+    schedule_toleration = k8s.client.V1Toleration(
+        key="worker-pool", operator="Equal", value="true", effect="NoSchedule"
+    )
+
+    secret = k8s.client.V1SecretVolumeSource(default_mode=420, secret_name="cloudvolume-secrets")
+    volume0 = k8s.client.V1Volume(name="cloudvolume-secrets", secret=secret)
+    volume1 = k8s.client.V1Volume(
+        name="dshm", empty_dir=k8s.client.V1EmptyDirVolumeSource(medium="Memory")
+    )
+    volume2 = k8s.client.V1Volume(
+        name="tmp", empty_dir=k8s.client.V1EmptyDirVolumeSource(medium="Memory")
+    )
+
+    pod_spec = k8s.client.V1PodSpec(
+        containers=[container],
+        dns_policy="Default",
+        restart_policy="Always",
+        scheduler_name="default-scheduler",
+        security_context={},
+        termination_grace_period_seconds=30,
+        tolerations=[schedule_toleration],
+        volumes=[volume0, volume1, volume2],
+    )
+
+    deployment_template = k8s.client.V1PodTemplateSpec(
+        metadata=k8s.client.V1ObjectMeta(labels=labels, creation_timestamp=None),
+        spec=pod_spec,
+    )
+
+    deployment_spec = k8s.client.V1DeploymentSpec(
+        progress_deadline_seconds=600,
+        replicas=replicas,
+        revision_history_limit=10,
+        selector=k8s.client.V1LabelSelector(match_labels=labels),
+        strategy=k8s.client.V1DeploymentStrategy(
+            type="RollingUpdate",
+            rolling_update=k8s.client.V1RollingUpdateDeployment(
+                max_surge="25%", max_unavailable="25%"
+            ),
+        ),
+        template=deployment_template,
+    )
+
+    deployment = k8s.client.V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=k8s.client.V1ObjectMeta(name=name, labels=labels),
+        spec=deployment_spec,
+    )
+
+    return deployment
 
 
 @builder.register("worker_k8s_deployment_ctx_mngr")

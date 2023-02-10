25e7b20b3d8f686bc2513eb9bb768a76241ac4ff
Nico Kemnitz <nico@zetta.ai>
2023-02-10 13:15:11 +0100

    feat: ChainIndexer to chain SampleIndexers

--- zetta_utils/training/datasets/sample_indexers/__init__.py
+++ zetta_utils/training/datasets/sample_indexers/__init__.py
@@ -4,6 +4,7 @@ Mappings between integer index id and the corresponding index for querying data.
 
 from . import base, random_indexer, volumetric_strided_indexer
 from .base import SampleIndexer
+from .chain_indexer import ChainIndexer
 from .random_indexer import RandomIndexer
 from .volumetric_strided_indexer import VolumetricStridedIndexer
 from .loop_indexer import LoopIndexer

5664f31a6b6c52667bf480dd593dfb536dffd70a
Nico Kemnitz <nico@zetta.ai>
2023-02-11 10:58:27 +0100

    inference model

--- zetta_utils/alignment/__init__.py
+++ zetta_utils/alignment/__init__.py
@@ -2,5 +2,6 @@ from . import field_profilers
 from . import online_finetuner
 from . import aced_relaxation
 from .base_encoder import BaseEncoder
+from .base_encoder_coarsener_chunked import BaseEncoderCoarsenerChunked
 from .encoding_coarsener import EncodingCoarsener
 from .misalignment_detector import MisalignmentDetector

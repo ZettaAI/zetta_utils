02cc834c5ab2cca1b23cb1bd91f14ec929f65616
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-02-24 22:38:57 +0000

    feat: blendable->volumetric; accepts vol layer sets

--- zetta_utils/mazepa_layer_processing/common/__init__.py
+++ zetta_utils/mazepa_layer_processing/common/__init__.py
@@ -1,7 +1,7 @@
 from .chunked_apply_flow import ChunkedApplyFlowSchema, build_chunked_apply_flow
-from .blendable_apply_flow import (
-    BlendableApplyFlowSchema,
-    build_blendable_apply_flow,
+from .volumetric_apply_flow import (
+    VolumetricApplyFlowSchema,
+    build_volumetric_apply_flow,
 )
 from .subchunkable_apply_flow import (
     build_subchunkable_apply_flow,
@@ -14,7 +14,7 @@ from .volumetric_callable_operation import (
     VolumetricCallableOperation,
     build_chunked_volumetric_callable_flow_schema,
 )
-from .. import ChunkableOpProtocol, BlendableOpProtocol
+from .. import ChunkableOpProtocol, VolumetricOpProtocol
 from .write_flow import build_write_flow, generic_write_flow
 from .interpolate_flow import build_interpolate_flow
 from .apply_mask_flow import build_apply_mask_flow

278922ea62773dd828df84da6f4a18c4c5004d99
Akhilesh Halageri <akhilesh@zetta.ai>
2023-02-25 18:18:18 +0000

    fix: logs, track sqs queues

--- zetta_utils/mazepa_addons/resource_allocation/aws_sqs.py
+++ zetta_utils/mazepa_addons/resource_allocation/aws_sqs.py
@@ -5,14 +5,17 @@ import boto3
 
 from zetta_utils import builder, log
 
+from .resource_tracker import ExecutionResource, register_execution_resource
+
 logger = log.get_logger("zetta_utils")
 
 
 @builder.register("sqs_queue_ctx_mngr")
 @contextmanager
-def sqs_queue_ctx_mngr(name: str):
+def sqs_queue_ctx_mngr(execution_id: str, name: str):
     sqs = boto3.resource("sqs")
     queue = sqs.create_queue(QueueName=name, Attributes={"SqsManagedSseEnabled": "false"})
+    register_execution_resource(ExecutionResource(execution_id, "sqs_queue", name))
 
     logger.info(f"Created SQS queue with URL={queue.url}")
     try:

aacb84dc4bfe8d90a4d15e04cf4db80d9c8cd4db
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-02-25 17:44:39 +0000

    refactor: group alignmnet related regimes

--- zetta_utils/training/lightning/regimes/__init__.py
+++ zetta_utils/training/lightning/regimes/__init__.py
@@ -1,8 +1,4 @@
 from . import naive_supervised
-from . import encoding_coarsener
-from . import encoding_coarsener_highres
-from . import encoding_coarsener_gen_x1
 from . import noop
-from . import base_encoder
-from . import misalignment_detector
-from . import misalignment_detector_aced
+
+from . import alignment
---
+++ zetta_utils/training/lightning/regimes/alignment/__init__.py
@@ -0,0 +1,6 @@
+from . import encoding_coarsener
+from . import encoding_coarsener_highres
+from . import encoding_coarsener_gen_x1
+from . import base_encoder
+from . import misalignment_detector
+from . import misalignment_detector_aced

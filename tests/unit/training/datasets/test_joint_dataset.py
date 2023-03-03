c83a002b5598fc65fe729928687e5707e638282b
Nico Kemnitz <nico@zetta.ai>
2023-03-03 15:24:40 +0100

    feat(joint_dataset): Shuffle samples

--- tests/unit/training/datasets/test_joint_dataset.py
+++ tests/unit/training/datasets/test_joint_dataset.py
@@ -11,6 +11,7 @@ def test_joint_dataset_constructor(mocker):
 
     JointDataset("horizontal", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m})
     JointDataset("vertical", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m})
+    JointDataset("vertical", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m}, sampling_order="shuffle")
 
 
 def test_joint_dataset_len(mocker):
@@ -22,9 +23,11 @@ def test_joint_dataset_len(mocker):
 
     jds_h = JointDataset("horizontal", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m})
     jds_v = JointDataset("vertical", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m})
+    jds_vs = JointDataset("vertical", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m}, sampling_order="shuffle")
 
     assert len(jds_h) == 2
     assert len(jds_v) == 5
+    assert len(jds_vs) == 5
 
 
 def test_joint_dataset_getitem(mocker):
@@ -37,8 +40,12 @@ def test_joint_dataset_getitem(mocker):
     layer_dataset1_m.__getitem__ = mocker.Mock(return_value=42)
     layer_dataset2_m.__getitem__ = mocker.Mock(return_value=57)
 
+    mocker.patch("random.shuffle", lambda x: x.reverse())
+
     jds_h = JointDataset("horizontal", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m})
     jds_v = JointDataset("vertical", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m})
+    jds_vs = JointDataset("vertical", {"lds1": layer_dataset1_m, "lds2": layer_dataset2_m}, sampling_order="shuffle")
 
     assert jds_h[0] == {"lds1": 42, "lds2": 57}
     assert [jds_v[i] for i in range(5)] == [42, 42, 57, 57, 57]
+    assert [jds_vs[i] for i in range(5)] == [57, 57, 57, 42, 42]
--- zetta_utils/training/datasets/joint_dataset.py
+++ zetta_utils/training/datasets/joint_dataset.py
@@ -1,3 +1,4 @@
+import random
 from typing import Any, Dict, Literal
 
 import attrs
@@ -11,17 +12,21 @@ logger = log.get_logger("zetta_utils")
 
 @builder.register("JointDataset")
 @typechecked
-@attrs.frozen
+@attrs.mutable
 class JointDataset(torch.utils.data.Dataset):
-    """PyTorch dataset wrapper to allow using multiple ``torch.utils.data.Dataset`` datasets
+    """PyTorch dataset wrapper to allow using multiple :class:`torch.utils.data.Dataset` datasets
     simultaneously.
 
     :param mode: String indicating whether the dataset is horizontally or vertically joined.
         ``horizontal`` means that the LayerDatasets will be sampled all at once and returned
         in a dictionary.
-        ``vertical`` means that the LayerDatasets will be sampled one after the other in the order
-        given during initialization.
+        ``vertical`` means that the LayerDatasets will be treated as a single, large dataset.
     :param datasets: Dictionary containing the datasets that make up the JointDataset.
+    :param sampling_order: By default, samples will be drawn starting from the first dataset in the
+    dictionary until all its samples are exhausted, before moving on to the next dataset.
+        ``ascending`` is the default behavior, with samples drawn from one dataset after another.
+        ``shuffle`` will randomly draw a sample (without replacement) from all available samples
+        across all datasets.
 
     """
 
@@ -29,13 +34,15 @@ class JointDataset(torch.utils.data.Dataset):
     datasets: Dict[str, Any]
     # TODO: Make torch.utils.data.Dataset pass mypy checks
     # datasets: Dict[str, torch.utils.data.Dataset]
+    sampling_order: Literal["ascending", "shuffle"] = "ascending"
+    _order: list[int] = attrs.field(init=False)
 
     def __attrs_pre_init__(self):
         super().__init__()
 
     def __attrs_post_init__(self):
+        num_samples = len(self)
         if self.mode == "horizontal":
-            num_samples = min([len(d) for d in self.datasets.values()])
             for key in self.datasets.keys():
                 if num_samples == len(self.datasets[key]):
                     logger.warning(
@@ -49,6 +56,10 @@ class JointDataset(torch.utils.data.Dataset):
                         f"but only {num_samples} samples will be used."
                     )
 
+        self._order = list(range(num_samples))
+        if self.sampling_order == "shuffle":
+            random.shuffle(self._order)
+
     def __len__(self) -> int:
         if self.mode == "horizontal":
             num_samples = min([len(d) for d in self.datasets.values()])
@@ -61,6 +72,9 @@ class JointDataset(torch.utils.data.Dataset):
         return num_samples
 
     def __getitem__(self, idx: int) -> Any:
+        if self.sampling_order == "shuffle":
+            idx = self._order[idx]
+
         if self.mode == "horizontal":
             sample = {}
             for key, dset in self.datasets.items():

198d8cb0f5ee1de6fd57eafa081b732afaef832f
Kisuk Lee <kisuk@zetta.ai>
2023-03-21 18:35:23 +0900

    feat: add crop_center to tensor_ops

--- zetta_utils/tensor_ops/__init__.py
+++ zetta_utils/tensor_ops/__init__.py
@@ -1,6 +1,15 @@
 from . import traceback_supress
 
 from . import common, convert, mask, transform
-from .common import InterpolationMode, compare, crop, interpolate, squeeze, unsqueeze, unsqueeze_to
+from .common import (
+    InterpolationMode,
+    compare,
+    crop,
+    crop_center,
+    interpolate,
+    squeeze,
+    unsqueeze,
+    unsqueeze_to,
+)
 from .convert import astype, to_np, to_torch
 from .mask import filter_cc  # , coarsen

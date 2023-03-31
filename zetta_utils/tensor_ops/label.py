f8a614cffb5f18f8bd0a09527216b17fab607903
Kisuk Lee <kisuk@zetta.ai>
2023-03-31 10:52:40 +0900

    feat: add utility tensor_ops for handling affinity

---
+++ zetta_utils/tensor_ops/label.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from typing import Sequence
+
+import numpy as np
+from typeguard import typechecked
+
+from zetta_utils import builder
+from zetta_utils.geometry import IntVec3D
+from zetta_utils.tensor_typing import TensorTypeVar
+
+from . import convert
+
+
+@builder.register("get_disp_pair")
+@typechecked
+def get_disp_pair(
+    data: TensorTypeVar,
+    disp: Sequence[int],
+) -> tuple[TensorTypeVar, TensorTypeVar]:
+    """
+    Get a pair of same-sized volumes displaced with respect to each other by
+    the given displacement vector.
+
+    :param data: Input segmentation
+    :param disp: Displacement vector
+    """
+    ndim = len(disp)
+    assert ndim == 3
+    disp = IntVec3D(*disp)
+
+    assert data.ndim >= ndim
+    for a, b in zip(data.shape[-ndim:], np.absolute(disp)):
+        assert a > b
+
+    disp1 = np.maximum(disp, 0)
+    disp2 = np.maximum(-disp, 0)
+
+    slices1 = [slice(0, None) for _ in range(data.ndim - ndim)]
+    slices2 = [slice(0, None) for _ in range(data.ndim - ndim)]
+    for shape, offset1, offset2 in zip(data.shape[-ndim:], disp1, disp2):
+        slices1.append(slice(offset1, shape - offset2))
+        slices2.append(slice(offset2, shape - offset1))
+
+    return data[tuple(slices1)], data[tuple(slices2)]
+
+
+@builder.register("seg_to_aff")
+@typechecked
+def seg_to_aff(
+    data: TensorTypeVar,
+    edge: Sequence[int],
+    mask: TensorTypeVar | None = None,
+) -> TensorTypeVar | tuple[TensorTypeVar, TensorTypeVar]:
+    """
+    Transform a segmentation into an affinity map characterized by the given
+    ``edge,'' or an offset vector.
+
+    :param data: Input segmentation
+    :param edge: Edge, or an offset vector
+    :param mask: Binary mask for `data`
+    """
+    pair = get_disp_pair(data, edge)
+    aff = (pair[0] == pair[1]) & (pair[0] != 0) & (pair[1] != 0)
+    aff = convert.astype(aff, data, cast=True)
+
+    if mask is not None:
+        assert data.shape == mask.shape
+        pair = get_disp_pair(mask, edge)
+        affmsk = convert.astype(pair[0] * pair[1], mask, cast=True)
+        return aff, affmsk
+
+    return aff

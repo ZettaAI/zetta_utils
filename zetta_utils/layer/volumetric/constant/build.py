ff36286bc1c1391afce167fe4b5de99fafdb0ffa
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-04-05 18:17:49 +0000

    feat: constant volumetric layer

---
+++ zetta_utils/layer/volumetric/constant/build.py
@@ -0,0 +1,71 @@
+# pylint: disable=missing-docstring
+from __future__ import annotations
+
+from typing import Iterable, Sequence, Union
+
+import torch
+
+from zetta_utils import builder
+from zetta_utils.tensor_ops import InterpolationMode
+
+from ... import DataProcessor, IndexProcessor, JointIndexDataProcessor
+from .. import VolumetricIndex, VolumetricLayer, build_volumetric_layer
+from . import ConstantVolumetricBackend
+
+# from typeguard import typechecked
+
+
+@builder.register("build_constant_volumetric_layer")
+def build_constant_volumetric_layer(  # pylint: disable=too-many-locals
+    value: float = 0,
+    num_channels: int = 1,
+    default_desired_resolution: Sequence[float] | None = None,
+    index_resolution: Sequence[float] | None = None,
+    data_resolution: Sequence[float] | None = None,
+    interpolation_mode: InterpolationMode | None = None,
+    allow_slice_rounding: bool = False,
+    index_procs: Iterable[IndexProcessor[VolumetricIndex]] = (),
+    read_procs: Iterable[
+        Union[
+            DataProcessor[torch.Tensor],
+            JointIndexDataProcessor[torch.Tensor, VolumetricIndex],
+        ]
+    ] = (),
+) -> VolumetricLayer:  # pragma: no cover # trivial conditional, delegation only
+    """Build a layer based on ConstantVolumetricBackend.
+
+    :param value: Value to fill read tensor with..
+    :param num_channels: Number of channels for the read tensor.
+    :param default_desired_resolution: Default resolution used when the desired resolution
+        is not given as a part of an index.
+    :param index_resolution: Resolution at which slices of the index will be given.
+    :param data_resolution: Resolution at which data will be read from the CloudVolume backend.
+        When ``data_resolution`` differs from ``desired_resolution``, data will be interpolated
+        from ``data_resolution`` to ``desired_resolution`` using the given ``interpolation_mode``.
+    :param interpolation_mode: Specification of the interpolation mode to use when
+        ``data_resolution`` differs from ``desired_resolution``.
+    :param allow_slice_rounding: Whether layer allows IO operations where the specified index
+        corresponds to a non-integer number of pixels at the desired resolution. When
+        ``allow_slice_rounding == True``, shapes will be rounded to nearest integer.
+    :param index_procs: List of processors that will be applied to the index given by the user
+        prior to IO operations.
+    :param read_procs: List of processors that will be applied to the read data before
+        returning it to the user.
+    """
+    backend = ConstantVolumetricBackend(
+        value=value,
+        num_channels=num_channels,
+    )
+
+    result = build_volumetric_layer(
+        backend=backend,
+        default_desired_resolution=default_desired_resolution,
+        index_resolution=index_resolution,
+        data_resolution=data_resolution,
+        interpolation_mode=interpolation_mode,
+        readonly=True,
+        allow_slice_rounding=allow_slice_rounding,
+        index_procs=index_procs,
+        read_procs=read_procs,
+    )
+    return result

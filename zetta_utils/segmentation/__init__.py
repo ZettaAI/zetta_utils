6517e2b928e39f68ed3d146f621a4c35f75f2673
Kisuk Lee <kisuk@zetta.ai>
2023-04-10 22:17:14 +0900

    feat: metric learning with edge loss

--- zetta_utils/segmentation/__init__.py
+++ zetta_utils/segmentation/__init__.py
@@ -1,5 +1,6 @@
 from .affinity import AffinityLoss, AffinityProcessor
 from .balance import BinaryClassBalancer
 from .common import MultiHeadedProcessor
+from .embedding import *
 from .inference import run_affinities_inference_onnx
 from .loss import *
---
+++ zetta_utils/segmentation/embedding/__init__.py
@@ -0,0 +1,2 @@
+from .common import EmbeddingProcessor
+from .edge import EdgeLoss

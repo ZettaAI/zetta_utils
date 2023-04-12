c7b107bd986a0f194eaf128b9aebbf10f0c7bea5
Kisuk Lee <kisuk@zetta.ai>
2023-04-12 17:26:36 +0900

    feat: add MultiHeadedProcessor

--- zetta_utils/segmentation/__init__.py
+++ zetta_utils/segmentation/__init__.py
@@ -1,4 +1,5 @@
 from .affinity import AffinityLoss, AffinityProcessor
 from .balance import BinaryClassBalancer
+from .common import MultiHeadedProcessor
 from .inference import run_affinities_inference_onnx
 from .loss import *

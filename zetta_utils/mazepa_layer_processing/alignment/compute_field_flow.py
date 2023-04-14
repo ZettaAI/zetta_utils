78bdcd8c20966b529dd9fb041ca814467a68f2ce
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-04-14 22:33:05 +0000

    feat: ComputeField to skip on empty `srs`; multistage subchunkable

--- zetta_utils/mazepa_layer_processing/alignment/compute_field_flow.py
+++ zetta_utils/mazepa_layer_processing/alignment/compute_field_flow.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import copy
-from typing import Optional, Protocol, Sequence
+from typing import Literal, Optional, Protocol, Sequence, Union
 
 import attrs
 import einops
@@ -86,46 +86,57 @@ class ComputeFieldOperation:
             field=src_field,
             idx=idx_input_padded,
         )
-        tgt_data, tgt_field_data, _ = translation_adjusted_download(
-            src=tgt, field=tgt_field, idx=idx_input_padded
-        )
-
-        if tgt_field_data is not None:
-            tgt_field_data_zcxy = einops.rearrange(tgt_field_data, "C X Y Z -> Z C X Y")
-            tgt_data_zcxy = einops.rearrange(tgt_data, "C X Y Z -> Z C X Y")
-            tgt_nonz_zcxy = tgt_data_zcxy != 0
-            tgt_data_warped = tgt_field_data_zcxy.field().from_pixels()(  # type: ignore
-                tgt_data_zcxy.float()
+        if src_data.abs().sum() > 0:
+            tgt_data, tgt_field_data, _ = translation_adjusted_download(
+                src=tgt, field=tgt_field, idx=idx_input_padded
             )
-            tgt_nonz_warped = tgt_field_data_zcxy.field().from_pixels()(  # type: ignore
-                (tgt_nonz_zcxy != 0).float()
-            )
-            tgt_data_warped[tgt_nonz_warped < 0.1] = 0
-            tgt_data_final = einops.rearrange(
-                tgt_data_warped,
-                "Z C X Y -> C X Y Z",
-            )
-        else:
-            tgt_data_final = tgt_data
 
-        result_raw = self.fn(
-            src=src_data,
-            tgt=tgt_data_final,
-            src_field=src_field_data,
-        )
-        result = tensor_ops.crop(result_raw, crop=self.output_crop_px)
+            if tgt_field_data is not None:
+                tgt_field_data_zcxy = einops.rearrange(tgt_field_data, "C X Y Z -> Z C X Y")
+                tgt_data_zcxy = einops.rearrange(tgt_data, "C X Y Z -> Z C X Y")
+                tgt_nonz_zcxy = tgt_data_zcxy != 0
+                tgt_data_warped = tgt_field_data_zcxy.field().from_pixels()(  # type: ignore
+                    tgt_data_zcxy.float()
+                )
+                tgt_nonz_warped = tgt_field_data_zcxy.field().from_pixels()(  # type: ignore
+                    (tgt_nonz_zcxy != 0).float()
+                )
+                tgt_data_warped[tgt_nonz_warped < 0.1] = 0
+                tgt_data_final = einops.rearrange(
+                    tgt_data_warped,
+                    "Z C X Y -> C X Y Z",
+                )
+            else:
+                tgt_data_final = tgt_data
 
-        result[0] += src_translation[0]
-        result[1] += src_translation[1]
-        dst[idx] = result
+            result_raw = self.fn(
+                src=src_data,
+                tgt=tgt_data_final,
+                src_field=src_field_data,
+            )
+            result = tensor_ops.crop(result_raw, crop=self.output_crop_px)
+
+            result[0] += src_translation[0]
+            result[1] += src_translation[1]
+            dst[idx] = result
 
 
 @builder.register("ComputeFieldFlowSchema")
 @mazepa.flow_schema_cls
 @attrs.mutable
 class ComputeFieldFlowSchema:
-    chunk_size: Sequence[int]
     operation: ComputeFieldOperation
+
+    processing_chunk_sizes: Sequence[Sequence[int]]
+    processing_crop_pads: Sequence[int] | Sequence[Sequence[int]] = (0, 0, 0)
+    processing_blend_pads: Sequence[int] | Sequence[Sequence[int]] = (0, 0, 0)
+    processing_blend_modes: Union[
+        Literal["linear", "quadratic"], Sequence[Literal["linear", "quadratic"]]
+    ] = "linear"
+    level_intermediaries_dirs: Sequence[str | None] | None = None
+    max_reduction_chunk_sizes: Sequence[int] | Sequence[Sequence[int]] | None = None
+    expand_bbox: bool = False
+    shrink_processing_chunk: bool = False
 
     def flow(
         self,
@@ -156,8 +167,14 @@ class ComputeFieldFlowSchema:
             src_field = src_field.with_procs(index_procs=(src_offsetter,) + src_field.index_procs)
         cf_flow = build_subchunkable_apply_flow(
             op=self.operation,  # type: ignore
-            processing_chunk_sizes=[self.chunk_size],
-            expand_bbox=True,
+            processing_chunk_sizes=self.processing_chunk_sizes,
+            processing_crop_pads=self.processing_crop_pads,
+            processing_blend_pads=self.processing_blend_pads,
+            processing_blend_modes=self.processing_blend_modes,
+            expand_bbox=self.expand_bbox,
+            shrink_processing_chunk=self.shrink_processing_chunk,
+            max_reduction_chunk_sizes=self.max_reduction_chunk_sizes,
+            level_intermediaries_dirs=self.level_intermediaries_dirs,
             bbox=bbox,
             dst_resolution=dst_resolution,
             dst=dst,
@@ -168,30 +185,3 @@ class ComputeFieldFlowSchema:
         )
 
         yield cf_flow
-
-
-@builder.register("build_compute_field_flow")
-def build_compute_field_flow(
-    chunk_size: Sequence[int],
-    operation: ComputeFieldOperation,
-    bbox: BBox3D,
-    dst_resolution: Sequence[float],
-    dst: VolumetricLayer,
-    src: VolumetricLayer,
-    tgt: Optional[VolumetricLayer] = None,
-    src_field: Optional[VolumetricLayer] = None,
-    tgt_offset: Sequence[float] = (0, 0, 0),
-    src_offset: Sequence[float] = (0, 0, 0),
-) -> mazepa.Flow:
-    flow_schema = ComputeFieldFlowSchema(chunk_size=chunk_size, operation=operation)
-    flow = flow_schema(
-        bbox=bbox,
-        dst_resolution=dst_resolution,
-        dst=dst,
-        src=src,
-        tgt=tgt,
-        src_field=src_field,
-        tgt_offset=tgt_offset,
-        src_offset=src_offset,
-    )
-    return flow

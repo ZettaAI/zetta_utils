cb38b002cf38b8f83563fd698573b0bc64fe9ff7
Nico Kemnitz <nico@zetta.ai>
2023-04-19 10:47:46 +0200

    fix(augmentations): data got moved from kwargs to args in #338

--- tests/unit/augmentations/test_common.py
+++ tests/unit/augmentations/test_common.py
@@ -9,9 +9,9 @@ def dummy_aug():
     return prob_aug(lambda x: x)
 
 
-def test_args_exc(dummy_aug):
+def test_args_with_data_kwarg_exc(dummy_aug):
     with pytest.raises(Exception):
-        dummy_aug("arg")
+        dummy_aug("input", "other", data="conflicting_data")
 
 
 def test_no_data_exc(dummy_aug):

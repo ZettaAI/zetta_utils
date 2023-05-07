f8eac2cb474f0f9a9103bf7fa6ad41a8fa8af36e
Akhilesh Halageri <akhilesh@zetta.ai>
2023-05-07 16:49:38 +0000

    reorganize k8s job

--- zetta_utils/mazepa_addons/resource_allocation/k8s/cronjob.py
+++ zetta_utils/mazepa_addons/resource_allocation/k8s/cronjob.py
@@ -13,6 +13,7 @@ from kubernetes import client as k8s_client  # type: ignore
 from zetta_utils import builder, log
 
 from .common import ClusterInfo, get_cluster_data
+from .job import get_job_template
 
 logger = log.get_logger("zetta_utils")
 
@@ -29,42 +30,17 @@ def _get_cronjob(
     labels: Optional[Dict[str, str]] = None,
 ) -> k8s_client.V1CronJob:
 
-    container = k8s_client.V1Container(
-        command=command,
-        args=command_args,
-        env=envs,
+    job_template = get_job_template(
         name=name,
+        namespace=namespace,
         image=image,
-        image_pull_policy="IfNotPresent",
-        resources=k8s_client.V1ResourceRequirements(
-            requests=resources,
-            limits=resources,
-        ),
-        termination_message_path="/dev/termination-log",
-        termination_message_policy="File",
-        volume_mounts=[],
+        command=command,
+        command_args=command_args,
+        envs=envs,
+        resources=resources,
+        labels=labels,
     )
 
-    schedule_toleration = k8s_client.V1Toleration(
-        key="worker-pool", operator="Equal", value="true", effect="NoSchedule"
-    )
-
-    pod_spec = k8s_client.V1PodSpec(
-        containers=[container],
-        dns_policy="Default",
-        restart_policy="OnFailure",
-        scheduler_name="default-scheduler",
-        security_context={},
-        termination_grace_period_seconds=30,
-        tolerations=[schedule_toleration],
-    )
-
-    common_meta = k8s_client.V1ObjectMeta(name=name, namespace=namespace, labels=labels)
-
-    pod_template = k8s_client.V1PodTemplateSpec(metadata=common_meta, spec=pod_spec)
-    job_spec = k8s_client.V1JobSpec(template=pod_template)
-
-    job_template = k8s_client.V1JobTemplateSpec(metadata=common_meta, spec=job_spec)
     cronjob_spec = k8s_client.V1CronJobSpec(
         concurrency_policy=spec_config.concurrency_policy,
         failed_jobs_history_limit=spec_config.failed_jobs_history_limit,
@@ -76,7 +52,7 @@ def _get_cronjob(
         time_zone=spec_config.time_zone,
     )
 
-    return k8s_client.V1CronJob(metadata=common_meta, spec=cronjob_spec)
+    return k8s_client.V1CronJob(metadata=job_template.metadata, spec=cronjob_spec)
 
 
 @builder.register("mazepa.k8s.CronJobSpec")

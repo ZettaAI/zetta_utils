ebe65a6bad3a0882efe9e83be4fc5e6317012647
Thomas Macrina <tmacrina@zetta.ai>
2023-05-17 22:47:31 +0100

    feat: preliminary support upsampling for uint64 data

--- zetta_utils/layer/volumetric/cloudvol/backend.py
+++ zetta_utils/layer/volumetric/cloudvol/backend.py
@@ -183,6 +183,8 @@ class CVBackend(VolumetricBackend):  # pylint: disable=too-few-public-methods
         # Enable autocrop for writes only
         cvol.autocrop = True
 
+        if (cvol.dtype == "uint64") and (data_final.dtype == np.int64):
+            data_final = data_final.astype(np.uint64)
         cvol[slices] = data_final
         cvol.autocrop = False
 

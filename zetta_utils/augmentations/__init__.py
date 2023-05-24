5e827927f71675b9cc43eb2e1e9392a87d37a0e4
Kisuk Lee <kisuk@zetta.ai>
2023-05-24 17:45:58 +0900

    feat: random noise augment

--- zetta_utils/augmentations/__init__.py
+++ zetta_utils/augmentations/__init__.py
@@ -1,4 +1,5 @@
 from . import tensor
 from .common import prob_aug, ComposedAugment
 from .imgaug import imgaug_augment
+from .noise import RandomGaussianNoise
 from .simple import build_simple_augment

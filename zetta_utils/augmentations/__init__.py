394c5b88f5ce6973aeca4c18d7c145c69636b9d6
Kisuk Lee <kisuk@zetta.ai>
2023-06-14 12:23:50 +0900

    feat: box occlusion augmentation

--- zetta_utils/augmentations/__init__.py
+++ zetta_utils/augmentations/__init__.py
@@ -1,5 +1,6 @@
 from . import tensor
 from .blur import BlurrySectionAugment
+from .box import FillBoxAugment, BlurBoxAugment, NoiseBoxAugment
 from .common import prob_aug, ComposedAugment
 from .grayscale import GrayscaleJitter3D, GrayscaleJitter2D, PartialGrayscaleJitter2D
 from .missing import MissingSectionAugment, PartialMissingSectionAugment

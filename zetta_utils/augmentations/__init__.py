0e9004fd9b4dc37d4133b915079378161157df1f
Kisuk Lee <kisuk@zetta.ai>
2023-06-22 21:40:07 +0900

    refactor: random noise augment

--- zetta_utils/augmentations/__init__.py
+++ zetta_utils/augmentations/__init__.py
@@ -4,5 +4,5 @@ from .box import FillBoxAugment, BlurBoxAugment, NoiseBoxAugment
 from .common import prob_aug, ComposedAugment
 from .grayscale import GrayscaleJitter3D, GrayscaleJitter2D, PartialGrayscaleJitter2D
 from .missing import MissingSectionAugment, PartialMissingSectionAugment
-from .noise import AdditiveGaussianNoiseAugment
+from .noise import RandomGaussianNoise
 from .simple import build_simple_augment
--- zetta_utils/augmentations/noise.py
+++ zetta_utils/augmentations/noise.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from functools import partial
-from typing import Callable, Literal
+from typing import Callable
 
 import attrs
 import skimage
@@ -10,48 +10,41 @@ from typeguard import typechecked
 
 from zetta_utils import builder
 from zetta_utils.distributions import Distribution, to_distribution
-from zetta_utils.layer import JointIndexDataProcessor
-from zetta_utils.layer.volumetric import VolumetricIndex
 from zetta_utils.tensor_ops import convert
 
+from .common import DataAugment
 
-@builder.register("AdditiveGaussianNoiseAugment")
+
+@builder.register("RandomGaussianNoise")
 @typechecked
-@attrs.mutable
-class AdditiveGaussianNoiseAugment(JointIndexDataProcessor):
+@attrs.frozen
+class RandomGaussianNoise(DataAugment):
     key: str
-    scale: float | Distribution
+    std: float | Distribution
+
     mean: float = 0
 
-    scale_distr: Distribution = attrs.field(init=False)
-
-    prepared_aug: Callable | None = attrs.field(init=False, default=None)
+    std_distr: Distribution = attrs.field(init=False)
+    random_noise: Callable = attrs.field(init=False)
 
     def __attrs_post_init__(self):
-        self.scale_distr = to_distribution(self.scale)
+        object.__setattr__(self, "std_distr", to_distribution(self.std))
+        object.__setattr__(
+            self,
+            "random_noise",
+            partial(
+                skimage.util.random_noise,
+                mode="gaussian",
+                clip=True,
+                mean=self.mean,
+            ),
+        )
 
-    def process_index(
-        self, idx: VolumetricIndex, mode: Literal["read", "write"]
-    ) -> VolumetricIndex:
-        return idx
-
-    def process_data(
-        self, data: dict[str, torch.Tensor], mode: Literal["read", "write"]
-    ) -> dict[str, torch.Tensor]:
-        self.prepare()
-        assert self.prepared_aug is not None
+    def augment(self, data: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
         assert self.key in data
         raw = data[self.key]
         data_np = convert.to_np(raw)
-        assert data_np.min() >= -1 and data_np.max() <= 1
-        data[self.key] = convert.astype(self.prepared_aug(data_np), raw, cast=True)
+        assert data_np.min() >= 0 and data_np.max() <= 1
+        result = self.random_noise(data_np, var=self.std_distr() ** 2)
+        data[self.key] = convert.astype(result, raw, cast=True)
         return data
-
-    def prepare(self) -> None:
-        self.prepared_aug = partial(
-            skimage.util.random_noise,
-            mode="gaussian",
-            clip=True,
-            mean=self.mean,
-            var=self.scale_distr() ** 2,
-        )

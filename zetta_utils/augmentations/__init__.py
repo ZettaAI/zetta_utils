de95cfefce12432956eac3c34918778d22defb5f
Kisuk Lee <kisuk@zetta.ai>
2023-06-23 15:57:24 +0900

    refactor: missing section augment

--- zetta_utils/augmentations/__init__.py
+++ zetta_utils/augmentations/__init__.py
@@ -3,6 +3,6 @@ from .blur import BlurrySectionAugment
 from .box import FillBoxAugment, BlurBoxAugment, NoiseBoxAugment
 from .common import prob_aug, ComposedAugment
 from .grayscale import GrayscaleJitter3D, GrayscaleJitter2D, PartialGrayscaleJitter2D
-from .missing import MissingSectionAugment, PartialMissingSectionAugment
+from .missing import build_missing_section, build_partial_missing_section
 from .noise import RandomGaussianNoise
 from .simple import build_simple_augment

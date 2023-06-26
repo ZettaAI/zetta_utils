ef99c6963cc17a718df7fdda926101bbab8899bb
Kisuk Lee <kisuk@zetta.ai>
2023-06-26 10:33:26 +0900

    feat: grayscale jitter augment

--- zetta_utils/augmentations/__init__.py
+++ zetta_utils/augmentations/__init__.py
@@ -2,6 +2,11 @@ from . import tensor
 from .blur import build_blurry_section, build_partial_blurry_section
 from .common import prob_aug, ComposedAugment
 from .imgaug import imgaug_augment
+from .grayscale import (
+    GrayscaleJitter3D,
+    build_grayscale_jitter_2d,
+    build_partial_grayscale_jitter_2d,
+)
 from .missing import build_missing_section, build_partial_missing_section
 from .noise import RandomGaussianNoise
 from .simple import build_simple_augment

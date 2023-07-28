e5a62d3f5ed35df1f13c88f0d884891cbf03dee2
Akhilesh Halageri <akhilesh@zetta.ai>
2023-07-28 17:03:36 +0000

    wip: ddp remote launch

--- zetta_utils/cloud/resource_allocation/k8s/job.py
+++ zetta_utils/cloud/resource_allocation/k8s/job.py
@@ -1,9 +1,8 @@
-# pylint: disable=unused-argument
-
 """
 Helpers for k8s job.
 """
 
+import time
 from contextlib import contextmanager
 from typing import Dict, List, Optional
 
@@ -12,12 +11,13 @@ from kubernetes import client as k8s_client  # type: ignore
 # from kubernetes import watch
 from zetta_utils import log
 
+from .common import ClusterInfo, get_cluster_data
+
 # from ..resource_tracker import (
 #     ExecutionResource,
 #     ExecutionResourceTypes,
 #     register_execution_resource,
 # )
-from .common import ClusterInfo, get_cluster_data
 from .secret import secrets_ctx_mngr
 
 logger = log.get_logger("zetta_utils")
@@ -95,34 +95,36 @@ def job_ctx_manager(
     k8s_client.Configuration.set_default(configuration)
 
     batch_v1_api = k8s_client.BatchV1Api()
-
     with secrets_ctx_mngr(execution_id, secrets, cluster_info):
         logger.info(f"Creating k8s job `{job.metadata.name}`")
         batch_v1_api.create_namespaced_job(body=job, namespace=namespace)
 
         try:
             yield
+            while True:
+                job = batch_v1_api.read_namespaced_job_status(
+                    name=job.metadata.name,
+                    namespace=namespace,
+                )
+                logger.info(f"`{job.metadata.name}` job status: {job.status}")
+                time.sleep(30)
+            # log_stream = watch.Watch().stream(
+            #     core_api.read_namespaced_pod_log,
+            #     name=f"job/{job.metadata.name}",
+            #     namespace=namespace,
+            # )
+            # for output in log_stream:
+            #     logger.info(output)
         finally:
-            ...
+            # new configuration to refresh expired tokens (long running executions)
+            configuration, _ = get_cluster_data(cluster_info)
+            k8s_client.Configuration.set_default(configuration)
 
-    # try:
-    #     yield
-    #     # core_api = k8s_client.CoreV1Api()
-    #     # log_stream = watch.Watch().stream(
-    #     #     core_api.read_namespaced_pod_log,
-    #     #     name=f"job/{job.metadata.name}",
-    #     #     namespace=namespace,
-    #     # )
-    #     # for output in log_stream:
-    #     #     logger.info(output)
-    # finally:
-    #     # new configuration to refresh expired tokens (long running executions)
-    #     configuration, _ = get_cluster_data(cluster_info)
-    #     k8s_client.Configuration.set_default(configuration)
-
-    #     # need to create a new client for the above to take effect
-    #     batch_v1_api = k8s_client.BatchV1Api()
-    #     logger.info(f"Deleting k8s job `{job.metadata.name}`")
-    #     batch_v1_api.delete_namespaced_job(
-    #         name=job.metadata.name, namespace=namespace
-    #     )
+            # need to create a new client for the above to take effect
+            batch_v1_api = k8s_client.BatchV1Api()
+            logger.info(f"Deleting k8s job `{job.metadata.name}`")
+            batch_v1_api.delete_namespaced_job(
+                name=job.metadata.name,
+                namespace=namespace,
+                propagation_policy="Foreground",
+            )

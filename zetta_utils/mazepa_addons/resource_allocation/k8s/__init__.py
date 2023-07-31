579746c1131d2389613937f346563bc3aaefdd86
Sergiy Popovych <sergiy.popovich@gmail.com>
2023-07-31 22:39:02 +0000

    resource allocation reference

--- zetta_utils/mazepa_addons/resource_allocation/k8s/__init__.py
+++ zetta_utils/mazepa_addons/resource_allocation/k8s/__init__.py
@@ -5,7 +5,7 @@ Tools to interact with kubernetes clusters.
 from .common import (
     ClusterInfo,
     get_cluster_data,
-    get_deployment,
+    get_mazepa_worker_deployment,
     get_secrets_and_mapping,
     deployment_ctx_mngr,
 )

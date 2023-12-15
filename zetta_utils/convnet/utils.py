175d781aeab11a476436f47203839c143310e56a
trivoldus28 <tri@zetta.ai>
2023-12-15 22:59:50 +0000

    Clean code and add test specs

--- zetta_utils/convnet/utils.py
+++ zetta_utils/convnet/utils.py
@@ -1,23 +1,27 @@
 from __future__ import annotations
 
 import io
-from typing import Optional, Sequence, Union
+from typing import Optional, Sequence, TypeAlias, Union
 
 import cachetools
 import fsspec
+import onnx
+import onnxruntime as ort
 import torch
 from typeguard import typechecked
 
-from zetta_utils import builder, log
+from zetta_utils import builder, log, tensor_ops
 
 logger = log.get_logger("zetta_utils")
+
+OrtInferenceSessionT: TypeAlias = ort.capi.onnxruntime_inference_collection.InferenceSession
 
 
 @builder.register("load_model")
 @typechecked
 def load_model(
     path: str, device: Union[str, torch.device] = "cpu", use_cache: bool = False
-) -> torch.nn.Module:  # pragma: no cover
+) -> torch.nn.Module | OrtInferenceSessionT:  # pragma: no cover
     if use_cache:
         result = _load_model_cached(path, device)
     else:
@@ -27,13 +31,17 @@ def load_model(
 
 def _load_model(
     path: str, device: Union[str, torch.device] = "cpu"
-) -> torch.nn.Module:  # pragma: no cover
+) -> torch.nn.Module | OrtInferenceSessionT:  # pragma: no cover
     logger.debug(f"Loading model from '{path}'")
     if path.endswith(".json"):
         result = builder.build(path=path).to(device)
     elif path.endswith(".jit"):
         with fsspec.open(path, "rb") as f:
             result = torch.jit.load(f, map_location=device)
+    elif path.endswith(".onnx"):
+        providers = ["CUDAExecutionProvider"] if device == "cuda" else ["CPUExecutionProvider"]
+        with fsspec.open(path, "rb") as f:
+            result = ort.InferenceSession(onnx.load(f).SerializeToString(), providers=providers)
     else:
         raise ValueError(f"Unsupported file format: {path}")
 
@@ -82,3 +90,30 @@ def load_weights_file(
             }
         model.load_state_dict(loaded_state, strict=strict)
     return model
+
+
+@typechecked
+def load_and_run_model(
+    path: str,
+    data_in: torch.Tensor,
+    device: Union[str, torch.device, None] = None,
+    use_cache: bool = True,
+) -> torch.Tensor:  # pragma: no cover
+
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    autocast_device = device.type if isinstance(device, torch.device) else str(device)
+
+    model = load_model(path=path, device=device, use_cache=use_cache)
+
+    if path.endswith(".onnx"):
+        data_in_np = data_in.numpy()
+        # No idea why I'm getting `error: "Tensor" not callable  [operator]` here
+        # reveal_type(data_in) = torch._tensor.Tensor
+        output_np = model.run(None, {"input": data_in_np})[0]  # type: ignore
+        output = tensor_ops.convert.to_torch(output_np)
+    else:
+        with torch.autocast(device_type=autocast_device):
+            output = model(data_in.to(device))
+    return output
--- zetta_utils/segmentation/affs_inferencer.py
+++ zetta_utils/segmentation/affs_inferencer.py
@@ -1,76 +1,153 @@
+from typing import Sequence
+
 import attrs
 import einops
+import numpy as np
 import torch
+from torch.nn import functional as F
 from typeguard import typechecked
 
 from zetta_utils import builder, convnet
 
-import cachetools
-import fsspec
-import numpy as np
-import onnx
-import onnxruntime as ort
-
-_session_cache: cachetools.LRUCache = cachetools.LRUCache(maxsize=1)
-
-@cachetools.cached(_session_cache)
-def _get_session(model_path: str):  # pragma: no cover
-    with fsspec.open(model_path, "rb") as f:
-        if model_path.endswith(".onnx"):
-            onnx_model = onnx.load(f)
-    return ort.InferenceSession(
-        onnx_model.SerializeToString(), providers=["CUDAExecutionProvider"]
-    )
 
 @builder.register("AffinitiesInferencer")
 @typechecked
-@attrs.mutable
+@attrs.frozen
 class AffinitiesInferencer:
-    # Input uint8 [   0 .. 255]
-    # Output uint8 [   0 .. 255]
+    # Input uint8  [   0 .. 255]
+    # Output float [   0 .. 255]
 
     # Don't create the model during initialization for efficient serialization
     model_path: str
-    myelin_mask_threshold: float
+    output_channels: Sequence[int]
 
-    def __call__(self,
-                 image: torch.Tensor,
-                 image_mask: torch.Tensor,
-                 output_mask: torch.Tensor,
+    bg_mask_channel: int | None = None
+    bg_mask_threshold: float = 0.0
+    bg_mask_invert_threshold: bool = False
+
+    def __call__(
+        self,
+        image: torch.Tensor,
+        image_mask: torch.Tensor,
+        output_mask: torch.Tensor,
     ) -> torch.Tensor:
-        if torch.cuda.is_available():
-            device = "cuda"
-        else:
-            device = "cpu"
 
         if image.dtype == torch.uint8:
             data_in = image.float() / 255.0  # [0.0 .. 1.0]
         else:
             raise ValueError(f"Unsupported image dtype: {image.dtype}")
 
+        # mask input
         data_in = data_in * image_mask
         data_in = einops.rearrange(data_in, "C X Y Z -> C Z Y X")
+        data_in = data_in.unsqueeze(0).float()
 
-        if self.model_path.endswith(".onnx"):
-            model = _get_session(self.model_path)
-            output = model.run(None, {"input": data_in.unsqueeze(0).float().numpy()})[0]
-        else:
-            # load model during the call _with caching_
-            model = convnet.utils.load_model(
-                self.model_path, device=device, use_cache=True
-            )
-            with torch.autocast(device_type=device):
-                output = model(data_in.to(device))
+        data_out = convnet.utils.load_and_run_model(path=self.model_path, data_in=data_in)
 
-        aff = output[:, :3, ...]
-        msk = np.amax(output[:, 3:, ...], axis=-4, keepdims=True)
-        output = np.concatenate((aff, msk), axis=-4)
-        output = torch.Tensor(output[0])
-        output_aff = output[0:3, :, :, :]
-        output_mye_mask = output[3:, :, :, :] < self.myelin_mask_threshold
-        output = torch.Tensor(output_aff) * output_mye_mask
+        # Extract requested channels
+        arrays = []
+        for channel in self.output_channels:
+            arrays.append(data_out[:, channel, ...])
+        if self.bg_mask_channel is not None:
+            arrays.append(data_out[:, self.bg_mask_channel, ...])
+        data_out = torch.Tensor(np.stack(arrays, axis=1)[0])
 
+        # mask output with bg_mask
+        num_channels = len(self.output_channels)
+        output = data_out[0:num_channels, :, :, :]
+        if self.bg_mask_channel is not None:
+            if self.bg_mask_invert_threshold:
+                bg_mask = data_out[num_channels:, :, :, :] > self.bg_mask_threshold
+            else:
+                bg_mask = data_out[num_channels:, :, :, :] < self.bg_mask_threshold
+            output = torch.Tensor(output) * bg_mask
+
+        # mask output
         output = einops.rearrange(output, "C Z Y X -> C X Y Z")
         output = output * output_mask
 
-        return (output*255).type(torch.uint8)
+        return output
+
+
+@builder.register("vec2aff")
+@typechecked
+def vec2aff(
+    vec: torch.Tensor,
+    edges: Sequence[Sequence[int]] = ((1, 0, 0), (0, 1, 0), (0, 0, 1)),  # assume CXYZ
+    delta_d: float = 1.5,
+) -> torch.Tensor:
+
+    assert vec.ndimension() >= 4
+    assert len(edges) > 0
+
+    affs = []
+    for edge in edges:
+        aff = compute_affinity(*(get_pair(vec, edge)), delta_d=delta_d)  # type: ignore
+        pad = ()
+        for e in reversed(edge):
+            if e > 0:
+                pad += (e, 0) # type: ignore
+            else:
+                pad += (0, abs(e)) # type: ignore
+        affs.append(F.pad(aff, pad))
+
+    assert len(affs) > 0
+    for aff in affs:
+        assert affs[0].size() == aff.size()
+
+    return torch.cat(affs, dim=-4)
+
+
+def compute_affinity(
+    embd1: torch.Tensor,
+    embd2: torch.Tensor,
+    dim: int = -4,
+    keepdims: bool = True,
+    delta_d: float = 1.5,
+) -> torch.Tensor:
+    """Compute an affinity map from a pair of embeddings."""
+    norm = torch.norm(embd1 - embd2, p=1, dim=dim, keepdim=keepdims)
+    margin = (2 * delta_d - norm) / (2 * delta_d)
+    zero = torch.zeros(1).to(embd1.device, dtype=embd1.dtype)
+    result = torch.max(zero, margin) ** 2
+    return result
+
+
+def get_pair(arr, edge):
+    shape = arr.size()[-3:]
+    edge = np.array(edge)
+    os1 = np.maximum(edge, 0)
+    os2 = np.maximum(-edge, 0)
+    arr1 = arr[
+        ..., os1[0] : shape[0] - os2[0], os1[1] : shape[1] - os2[1], os1[2] : shape[2] - os2[2]
+    ]
+    arr2 = arr[
+        ..., os2[0] : shape[0] - os1[0], os2[1] : shape[1] - os1[1], os2[2] : shape[2] - os1[2]
+    ]
+    return arr1, arr2
+
+
+@builder.register("vec2aff_v1")
+@typechecked
+def vec2aff_v1(
+    embeddings,
+    offsets=(1, 1, 1),
+    delta_mult=15000,
+) -> torch.Tensor:
+    # Tri's naive implementation
+    metric_out = np.zeros(shape=(3,) + embeddings.shape[1:])
+    # compute mean-square
+    metric_out[0, offsets[0] :, :, :] = (
+        (embeddings[:, offsets[0] :, :, :] - embeddings[:, : -offsets[0], :, :]) ** 2
+    ).mean(axis=0)
+    metric_out[1, :, offsets[1] :, :] = (
+        (embeddings[:, :, offsets[1] :, :] - embeddings[:, :, : -offsets[1], :]) ** 2
+    ).mean(axis=0)
+    metric_out[2, :, :, offsets[2] :] = (
+        (embeddings[:, :, :, offsets[2] :] - embeddings[:, :, :, : -offsets[2]]) ** 2
+    ).mean(axis=0)
+    metric_out *= delta_mult
+    # convert to affinities
+    metric_out[metric_out > 1] = 1
+    metric_out_ = torch.Tensor(1.0 - metric_out)
+    return metric_out_

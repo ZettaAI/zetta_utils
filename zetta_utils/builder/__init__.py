b85e57e84316fe5b1e5a3ee549015de3287b72f6
Sergiy Popovych <sergiy.popovich@gmail.com>
2024-01-16 08:02:45 +0000

    chore: rename PARALLEL_BUILD_ALLOWED var

--- zetta_utils/builder/__init__.py
+++ zetta_utils/builder/__init__.py
@@ -4,4 +4,4 @@ from .registry import REGISTRY, register, get_matching_entry, unregister
 from .build import SPECIAL_KEYS, build, BuilderPartial, get_initial_builder_spec
 from . import built_in_registrations
 
-PARALLEL_BUILD_BY_DEFAULT: bool = False
+PARALLEL_BUILD_ALLOWED: bool = False

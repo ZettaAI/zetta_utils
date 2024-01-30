74842648e3a50de54c87eb6ef517a2940b360a01
Nico Kemnitz <nico@zetta.ai>
2024-01-30 11:29:46 +0100

    test(datastore_layer): Add pickle tests

--- tests/unit/layer/db_layer/datastore/test_backend.py
+++ tests/unit/layer/db_layer/datastore/test_backend.py
@@ -1,5 +1,8 @@
 # pylint: disable=redefined-outer-name
 
+import pickle
+from typing import cast
+
 import pytest
 
 from zetta_utils.layer.db_layer.datastore import DatastoreBackend, build_datastore_layer
@@ -55,3 +58,16 @@ def test_with_changes(datastore_emulator) -> None:
             project=backend.project,
             some_key="test",
         )
+
+
+def test_pickle(datastore_emulator) -> None:
+    layer = build_datastore_layer(datastore_emulator, datastore_emulator)
+    layer_backend = cast(DatastoreBackend, layer.backend)
+    assert layer_backend.client is not None  # ensure client is initialized
+
+    layer2 = pickle.loads(pickle.dumps(layer))
+    layer_backend2 = cast(DatastoreBackend, layer2.backend)
+    assert layer_backend2.project == layer_backend.project
+    assert layer_backend2.namespace == layer_backend.namespace
+    assert layer_backend2.exclude_from_indexes == layer_backend.exclude_from_indexes
+    assert layer_backend2.name == layer_backend.name

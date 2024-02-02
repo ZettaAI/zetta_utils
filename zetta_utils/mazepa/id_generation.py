3a8425d5c8f1a9605e783006cf8373136d5b8f59
Nico Kemnitz <nico@zetta.ai>
2024-02-02 10:20:00 +0100

    fix(subchunkable_apply_flow): generate deterministic flow_id

--- zetta_utils/mazepa/id_generation.py
+++ zetta_utils/mazepa/id_generation.py
@@ -36,11 +36,21 @@ def get_unique_id(
 
 
 def generate_invocation_id(
-    fn: Callable,
-    args: list,
-    kwargs: dict,
+    fn: Optional[Callable] = None,
+    args: Optional[list] = None,
+    kwargs: Optional[dict] = None,
     prefix: Optional[str] = None,
-):
+) -> str:
+    """Generate a unique and deterministic ID for a function invocation.
+    The ID is generated using xxhash and dill to hash the function and its arguments.
+
+    :param fn: the function, or really any Callable, defaults to None
+    :param args: the function arguments, or any list, defaults to None
+    :param kwargs: the function kwargs, or any dict, defaults to None
+    :param prefix: optional prefix str, separated by `-`, defaults to None
+    :return: A unique, yet deterministic string that identifies (fn, args, kwargs) in
+      the current Python environment.
+    """
     x = xxhash.xxh128()
     try:
         x.update(

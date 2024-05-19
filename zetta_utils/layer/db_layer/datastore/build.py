12f67eca7c0014ad615201bc29dff41f95a5ed14
Akhilesh Halageri <akhilesh@zetta.ai>
2024-05-19 16:11:35 +0000

    build ds layer database arg and exclude index cols arg

--- zetta_utils/layer/db_layer/datastore/build.py
+++ zetta_utils/layer/db_layer/datastore/build.py
@@ -1,8 +1,6 @@
 # pylint: disable=missing-docstring
 from __future__ import annotations
 
-from typing import Optional
-
 from typeguard import typechecked
 
 from zetta_utils import builder
@@ -15,19 +13,24 @@ from . import DatastoreBackend
 @builder.register("build_datastore_layer")
 def build_datastore_layer(
     namespace: str,
-    project: Optional[str] = None,
+    project: str | None = None,
+    database: str | None = None,
+    exclude_from_indexes: tuple[str, ...] = (),
     readonly: bool = False,
 ) -> DBLayer:
     """Build a Datastore Layer.
 
-    :param namespace: Namespace (database) to use.
+    :param namespace: Namespace (zutils database) to use.
     :param project: Google Cloud project ID.
+    :param database: GCP Datastore database to use.
+    :param exclude_from_indexes: Tuple of column names to not be indexed.
     :param readonly: Whether layer is read only.
 
     :return: Datastore Layer built according to the spec.
 
     """
 
-    backend = DatastoreBackend(namespace, project=project)
+    backend = DatastoreBackend(namespace, project=project, database=database)
+    backend.exclude_from_indexes = exclude_from_indexes
     result = build_db_layer(backend=backend, readonly=readonly)
     return result

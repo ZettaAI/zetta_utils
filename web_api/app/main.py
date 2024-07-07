1a8cd90006ded38e0586ae453d98f59d43d1fc77
Akhilesh Halageri <akhilesh@zetta.ai>
2024-07-07 22:03:47 +0000

    feat(web_api): cutout dl/ul

--- web_api/app/main.py
+++ web_api/app/main.py
@@ -8,6 +8,7 @@ from .annotations import api as annotations_api
 from .collections import api as collections_api
 from .layer_groups import api as layer_groups_api
 from .layers import api as layers_api
+from .painting import api as painting_api
 
 app = FastAPI()
 
@@ -23,6 +24,7 @@ app.mount("/annotations", annotations_api)
 app.mount("/collections", collections_api)
 app.mount("/layer_groups", layer_groups_api)
 app.mount("/layers", layers_api)
+app.mount("/painting", painting_api)
 
 
 @app.get("/")

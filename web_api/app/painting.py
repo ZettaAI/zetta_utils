496a3491c359de6f4ce615e4ab0e89a37cb745de
Akhilesh Halageri <akhilesh@zetta.ai>
2024-07-23 22:52:46 +0000

    add permute_pattern query param

--- web_api/app/painting.py
+++ web_api/app/painting.py
@@ -18,26 +18,30 @@ async def read_cutout(
     bbox_start: Annotated[tuple[int, int, int], Query()],
     bbox_end: Annotated[tuple[int, int, int], Query()],
     resolution: Annotated[tuple[float, float, float], Query()],
-    permute: Annotated[bool, Query()],
+    permute_pattern: Annotated[str | None, Query()] = None,
 ):
     index = VolumetricIndex.from_coords(bbox_start, bbox_end, Vec3D(*resolution))
     layer = build_cv_layer(path, readonly=True)
     data = np.ascontiguousarray(layer[index])
-    if permute:
-        data = einops.rearrange(data, "C X Y Z -> C Z Y X")
+    if permute_pattern:
+        data = einops.rearrange(data, permute_pattern)
     return Response(content=data.tobytes())
 
 
 @api.post("/cutout")
 async def write_cutout(
+    data: bytes,
     path: Annotated[str, Query()],
     bbox_start: Annotated[tuple[int, int, int], Query()],
     bbox_end: Annotated[tuple[int, int, int], Query()],
     resolution: Annotated[tuple[float, float, float], Query()],
-    data: bytes,
+    permute_pattern: Annotated[str | None, Query()] = None,
 ):
     index = VolumetricIndex.from_coords(bbox_start, bbox_end, Vec3D(*resolution))
     cv_kwargs = {"non_aligned_writes": True}
     layer = build_cv_layer(path, cv_kwargs=cv_kwargs)
     shape = np.array(bbox_end) - np.array(bbox_start)
-    layer[index] = np.frombuffer(data, dtype=layer.backend.dtype).reshape(shape)
+    data_arr = np.frombuffer(data, dtype=layer.backend.dtype).reshape(shape)
+    if permute_pattern:
+        data_arr = einops.rearrange(data_arr, permute_pattern)
+    layer[index] = data_arr

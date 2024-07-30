959928b5d03f673834ffe8b2a31b4651028bb767
zettavasyl <vasyl.perehinets@zetta.ai>
2024-07-30 15:55:15 +0300

    feat: :alien: updated write_cutout API

--- web_api/app/painting.py
+++ web_api/app/painting.py
@@ -35,14 +35,17 @@ async def write_cutout(
     bbox_start: Annotated[tuple[int, int, int], Query()],
     bbox_end: Annotated[tuple[int, int, int], Query()],
     resolution: Annotated[tuple[float, float, float], Query()],
-    permute_pattern: Annotated[str | None, Query()] = None,
+    is_fortran: Annotated[bool, Query()] = True,
 ):
     index = VolumetricIndex.from_coords(bbox_start, bbox_end, Vec3D(*resolution))
     cv_kwargs = {"non_aligned_writes": True}
     layer = build_cv_layer(path, cv_kwargs=cv_kwargs)
-    shape = np.array(bbox_end) - np.array(bbox_start)
+    shape = [layer.backend.num_channels, *(np.array(bbox_end) - np.array(bbox_start))]
     data = await request.body()
-    data_arr = np.frombuffer(data, dtype=layer.backend.dtype).reshape((1, *shape))
-    if permute_pattern:
-        data_arr = einops.rearrange(data_arr, permute_pattern)
+    if not is_fortran:
+        data_arr = np.frombuffer(data, dtype=layer.backend.dtype).reshape(shape)
+    else:
+        data_arr = np.frombuffer(data, dtype=layer.backend.dtype).reshape(shape[::-1])
+        data_arr = einops.rearrange(data_arr, "Z Y X C -> C X Y Z")
     layer[index] = data_arr
+   

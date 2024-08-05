1b5f1178fcc2a2ba5b3b55438e496bb2e479c6a9
Sergiy <sergiy.popovich@gmail.com>
2024-08-05 12:11:19 +0000

    removed auth

--- web_api/app/main.py
+++ web_api/app/main.py
@@ -32,32 +32,32 @@ app.mount("/layers", layers_api)
 app.mount("/painting", painting_api)
 
 
-@app.middleware("http")
-async def check_authorized_user(request: Request, call_next):
-    if request.method != "OPTIONS":
-        try:
-            token = request.headers["authorization"].split()[-1]
-        except (KeyError, IndexError):
-            return Response(content="Missing auth token.", status_code=401)
+# @app.middleware("http")
+# async def check_authorized_user(request: Request, call_next):
+#     if request.method != "OPTIONS":
+#         try:
+#             token = request.headers["authorization"].split()[-1]
+#         except (KeyError, IndexError):
+#             return Response(content="Missing auth token.", status_code=401)
 
-        client_id = os.environ["OAUTH_CLIENT_ID"]
-        try:
-            idinfo = id_token.verify_oauth2_token(token, requests.Request(), client_id)
-        except Exception as exc:  # pylint: disable=broad-exception-caught
-            return Response(content=str(exc), status_code=401)
+#         client_id = os.environ["OAUTH_CLIENT_ID"]
+#         try:
+#             idinfo = id_token.verify_oauth2_token(token, requests.Request(), client_id)
+#         except Exception as exc:  # pylint: disable=broad-exception-caught
+#             return Response(content=str(exc), status_code=401)
 
-        user = f"user:{idinfo['email']}"
-        client = iap_v1.IdentityAwareProxyAdminServiceClient()
+#         user = f"user:{idinfo['email']}"
+#         client = iap_v1.IdentityAwareProxyAdminServiceClient()
 
-        iap_resource = os.environ["IAP_RESOURCE"]
-        request = iam_policy_pb2.GetIamPolicyRequest(resource=iap_resource)
-        policy = client.get_iam_policy(request=request)
-        members = set(policy.bindings[0].members)
-        if user not in members:
-            return Response(content="User not authorized.", status_code=401)
+#         iap_resource = os.environ["IAP_RESOURCE"]
+#         request = iam_policy_pb2.GetIamPolicyRequest(resource=iap_resource)
+#         policy = client.get_iam_policy(request=request)
+#         members = set(policy.bindings[0].members)
+#         if user not in members:
+#             return Response(content="User not authorized.", status_code=401)
 
-    response = await call_next(request)
-    return response
+#     response = await call_next(request)
+#     return response
 
 
 @app.get("/")

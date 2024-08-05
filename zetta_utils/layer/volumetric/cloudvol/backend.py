9dce66d1058c806a1dd816bd217a737150cb467e
Sergiy <sergiy.popovich@gmail.com>
2024-08-05 12:35:32 +0000

    mm

--- zetta_utils/layer/volumetric/cloudvol/backend.py
+++ zetta_utils/layer/volumetric/cloudvol/backend.py
@@ -228,7 +228,6 @@ class CVBackend(VolumetricBackend):  # pylint: disable=too-few-public-methods
                 raise ValueError("Attempting to write negative values to a uint64 CloudVolume")
             data_final = data_final.astype(np.uint64)
         print (f"Non aligned writes: {cvol.non_aligned_writes}")
-        cvol.non_aligned_writes
         cvol[slices] = data_final
         cvol.autocrop = False
 

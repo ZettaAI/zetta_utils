212d5394d1aaac814fdf839457269bdfc4a1d43b
Sergiy <sergiy.popovich@gmail.com>
2024-09-10 16:38:08 +0000

    chore: tools for np-centric execution

--- zetta_utils/builder/__init__.py
+++ zetta_utils/builder/__init__.py
@@ -1,4 +1,7 @@
 """Building objects from specs"""
+import inspect
+from typing import List
+import numpy as np
 from . import constants
 from .registry import REGISTRY, register, get_matching_entry, unregister
 from .building import (
@@ -11,3 +14,8 @@ from .building import (
 from . import built_in_registrations
 
 PARALLEL_BUILD_ALLOWED: bool = False
+
+NP_MANUAL: List[str] = []
+for k in dir(np):
+    if not k.startswith("_") and k not in NP_MANUAL and inspect.isroutine(getattr(np, k)):
+        register(f"np.{k}")(getattr(np, k))
--- zetta_utils/convnet/simple_inference_runner.py
+++ zetta_utils/convnet/simple_inference_runner.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 import attrs
 import torch
+from numpy import typing as npt
 from typeguard import typechecked
 
 from zetta_utils import builder, convnet, tensor_ops
+from zetta_utils.tensor_ops.convert import to_np, to_torch
+from zetta_utils.tensor_typing import Tensor
 
 
 @builder.register("SimpleInferenceRunner")
@@ -15,7 +18,7 @@ class SimpleInferenceRunner:  # pragma: no cover
     model_path: str
     unsqueeze_to: int | None = None
 
-    def __call__(self, src: torch.Tensor) -> torch.Tensor:
+    def __call__(self, src: Tensor) -> npt.NDArray:
         if torch.cuda.is_available():
             device = "cuda"
         else:
@@ -23,7 +26,8 @@ class SimpleInferenceRunner:  # pragma: no cover
 
         # load model during the call _with caching_
         model = convnet.utils.load_model(self.model_path, device=device, use_cache=True)
+
         if self.unsqueeze_to is not None:
             src = tensor_ops.unsqueeze_to(src, self.unsqueeze_to)
-        result = model(src.to(device))
+        result = to_np(model(to_torch(src).to(device)))
         return result

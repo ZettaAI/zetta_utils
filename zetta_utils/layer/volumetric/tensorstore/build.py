e925f6c947a41f865e9ddc759973e74e386bfdca
trivoldus28 <tri@zetta.ai>
2025-01-23 19:25:24 +0000

    Fix missing new line

--- zetta_utils/layer/volumetric/tensorstore/build.py
+++ zetta_utils/layer/volumetric/tensorstore/build.py
@@ -14,6 +14,7 @@ from .. import VolumetricIndex, VolumetricLayer, build_volumetric_layer
 from ..precomputed import InfoExistsModes, PrecomputedInfoSpec
 from . import TSBackend
 
+
 # from typeguard import typechecked
 # @typechecked # ypeError: isinstance() arg 2 must be a type or tuple of types on p3.9
 @builder.register("build_ts_layer")

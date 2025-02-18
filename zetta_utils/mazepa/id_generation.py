14bf72ef523a2d9b4226d0e031629f4395d8e3ca
Dodam Ih <dodam@zetta.ai>
2025-02-18 14:48:17 -0800

    feat: temporary DBLayer

--- zetta_utils/mazepa/id_generation.py
+++ zetta_utils/mazepa/id_generation.py
@@ -6,33 +6,10 @@ from typing import Callable, Optional
 
 import dill
 import xxhash
-from coolname import generate_slug
 
 from zetta_utils import log
 
 logger = log.get_logger("mazepa")
-
-
-def get_unique_id(
-    slug_len: int = 3,
-    add_uuid: bool = True,
-    prefix: Optional[str] = None,
-    max_len: int = 320,
-) -> str:  # pragma: no cover
-    slug = generate_slug(slug_len)
-
-    if prefix is not None:
-        result = f"{prefix}-{slug}"
-    else:
-        result = f"{slug}"
-
-    if add_uuid:
-        unique_id = str(uuid.uuid1())
-        result += unique_id
-    result = result[:max_len]
-    while not result[-1].isalpha():
-        result = result[:-1]
-    return result
 
 
 def generate_invocation_id(

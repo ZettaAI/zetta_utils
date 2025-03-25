d5a9228f4b1a726c1ee30a97621a2500dc541866
Sergiy <sergiy.popovich@gmail.com>
2025-03-25 18:16:06 +0000

    hotfix: signal handler bug

--- zetta_utils/common/signal_handlers.py
+++ zetta_utils/common/signal_handlers.py
@@ -4,18 +4,13 @@ from contextlib import contextmanager
 
 @contextmanager
 def custom_signal_handler_ctx(fn, target_signal):  # pragma: no cover
-    # save original SIGINT handler
     original_sigint_handler = signal.getsignal(target_signal)  # eg signal.SIGINT
 
-    # install the new SIGINT handler
-    signal.signal(signal.SIGINT, fn)
+    signal.signal(target_signal, fn)
 
     try:
-        # whatever is yielded is what's picked up in the "as"
         yield
     except Exception as e:
         raise e
     finally:
-        # when leaving contextmanager, reinstall old SIGINT handler
-        # note: only works if old SIGINT handler was originally installed by Python
-        signal.signal(signal.SIGINT, original_sigint_handler)
+        signal.signal(target_signal, original_sigint_handler)

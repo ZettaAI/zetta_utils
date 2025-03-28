08bcc16c4f1da7e78d8c9d34117fd02567687850
Sergiy <sergiy.popovich@gmail.com>
2025-03-28 18:44:59 +0000

    feat: inferencer updates

--- zetta_utils/convnet/simple_inference_runner.py
+++ zetta_utils/convnet/simple_inference_runner.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import attrs
+import numpy as np
 import torch
 from numpy import typing as npt
 from typeguard import typechecked
@@ -16,7 +17,10 @@ from zetta_utils.tensor_typing import Tensor
 class SimpleInferenceRunner:  # pragma: no cover
     # Don't create the model during initialization for efficient serialization
     model_path: str
-    unsqueeze_to: int | None = None
+    apply_sigmoid: bool = False
+    is_3d: bool = False
+    skip_zeros: bool = True
+    output_channels: int | None = None
 
     def __call__(self, src: Tensor) -> npt.NDArray:
         if torch.cuda.is_available():
@@ -24,10 +28,21 @@ class SimpleInferenceRunner:  # pragma: no cover
         else:
             device = "cpu"
 
-        # load model during the call _with caching_
+        if self.is_3d:
+            src = tensor_ops.unsqueeze_to(src, 5)
+
+        if self.skip_zeros and (src != 0).sum() == 0:
+            output_shape = list(src.shape)
+            if self.output_channels is not None:
+                output_shape[1] = self.output_channels
+            return np.zeros(output_shape)
+
+        # load model during the call _with caching_\
         model = convnet.utils.load_model(self.model_path, device=device, use_cache=True)
 
-        if self.unsqueeze_to is not None:
-            src = tensor_ops.unsqueeze_to(src, self.unsqueeze_to)
-        result = to_np(model(to_torch(src).to(device)))
-        return result
+        result = model(to_torch(src).to(device))
+        if self.apply_sigmoid:
+            result = torch.sigmoid(result)
+
+        result_np = to_np(result)
+        return result_np

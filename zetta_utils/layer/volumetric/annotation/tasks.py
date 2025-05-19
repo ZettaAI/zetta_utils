b3034c9f7b2f45cc1746ccf1511c46103dccea31
JoeStrout <joe@strout.net>
2025-05-19 22:09:20 +0000

    Fix recursive import error, and remove just-added imports since they appear redundant.

--- zetta_utils/layer/volumetric/annotation/tasks.py
+++ zetta_utils/layer/volumetric/annotation/tasks.py
@@ -3,15 +3,18 @@ Module containing task and flow definitions for annotation layer operations.
 """
 
 from zetta_utils import builder, mazepa
-from zetta_utils.layer.volumetric import VolumetricAnnotationLayer
+
+# from zetta_utils.layer.volumetric import VolumetricAnnotationLayer
 
 
 @mazepa.taskable_operation
-def post_process_annotation_layer_op(target: VolumetricAnnotationLayer):  # pragma: no cover
+def post_process_annotation_layer_op(target):  # pragma: no cover
+    # target: VolumetricAnnotationLayer
     target.backend.post_process()
 
 
 @builder.register("post_process_annotation_layer_flow")
 @mazepa.flow_schema
-def post_process_annotation_layer_flow(target: VolumetricAnnotationLayer):  # pragma: no cover
+def post_process_annotation_layer_flow(target):  # pragma: no cover
+    # target: VolumetricAnnotationLayer
     yield post_process_annotation_layer_op.make_task(target)

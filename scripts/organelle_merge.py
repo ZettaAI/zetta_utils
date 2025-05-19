72affbe3e0da2bd7ea9a8dcefac3a95a0c87a682
JoeStrout <joe@strout.net>
2025-05-19 17:01:34 +0000

    Fix mypy issues in organelle_merge.py.

--- scripts/organelle_merge.py
+++ scripts/organelle_merge.py
@@ -9,26 +9,28 @@ import argparse
 import json
 import readline
 import struct
-import scipy
 import sys
+from binascii import unhexlify
+from math import floor
+from typing import Any, Optional, Sequence
+
 import numpy as np
-from google.cloud import storage
+import scipy
 from caveclient import CAVEclient, chunkedgraph
 from cloudvolume import CloudVolume
-from math import floor
-from typing import Any, Sequence
-from binascii import unhexlify
+from google.cloud import storage
+
 from zetta_utils.geometry import BBox3D, Vec3D
+from zetta_utils.geometry.mask_center import interior_point
 from zetta_utils.layer.volumetric import VolumetricIndex
 from zetta_utils.layer.volumetric.cloudvol import build_cv_layer
-from zetta_utils.layer.volumetric.precomputed import PrecomputedInfoSpec
-from zetta_utils.geometry.mask_center import interior_point
 
 # Globals
+client: Optional[CAVEclient] = None
 point_resolution = Vec3D(0, 0, 0)
-seg_vol = None
-seg_bounds = None
-args = None
+seg_vol: Optional[CloudVolume] = None
+seg_bounds: Optional[BBox3D] = None
+args: Optional[argparse.Namespace] = None
 seg_resolution = Vec3D(0, 0, 0)
 seg_chunk_size = Vec3D(0, 0, 0)
 seg_offset = Vec3D(0, 0, 0)
@@ -36,16 +38,16 @@ inp_to_seg = Vec3D(0.0, 0.0, 0.0)
 chunk = None
 chunk_bounds = None
 cg_client = None
-line_annotations = []   # lines representing merges
+line_annotations = []  # lines representing merges
 point_annotations = []  # points representing failed/aborted merges
-pcg_table_name = ''
+pcg_table_name = ""
 
-def verify_cave_auth() -> bool:
+
+def verify_cave_auth():
     # pylint: disable-next=global-statement
     global client
     client = CAVEclient(
-        datastack_name="dacey_human_fovea", 
-        server_address='https://proofreading.zetta.ai'
+        datastack_name="dacey_human_fovea", server_address="https://proofreading.zetta.ai"
     )
     try:
         # pylint: disable-next=pointless-statement    # bite me, pylint
@@ -77,7 +79,8 @@ def parse_wkb_hex_point(hex_str: str) -> tuple:
         except struct.error as exc:
             raise ValueError("Invalid WKB hex string") from exc
 
-def parse_point(point_string: str) -> Vec3D:
+
+def parse_point(point_string: str) -> Optional[Vec3D]:
     """This function attempts to parse a string as a 3D point.
     It supports any combination of spaces, commas, or vertical
     bars as field separators, and strips extra separators from
@@ -113,7 +116,7 @@ def read_gcs_json(bucket_path):
     return data
 
 
-def load_volume(path, scale_index=0):
+def load_volume(path, scale_index=0) -> tuple[CloudVolume, BBox3D]:
     """
     Load a CloudVolume given the path, and optionally, which scale (resolution) is desired.
     Return the CloudVolume, and a BBox3D describing the data bounds.
@@ -128,6 +131,7 @@ def load_volume(path, scale_index=0):
     end_coord = [a + b for (a, b) in zip(start_coord, size)]
     bounds = BBox3D.from_coords(start_coord, end_coord)
     return vol, bounds
+
 
 def safe_get_block(vol, x_slice, y_slice, z_slice):
     """
@@ -164,12 +168,13 @@ def safe_get_block(vol, x_slice, y_slice, z_slice):
 
     # Insert the valid block into the zero-padded array
     padded_block[
-        x_offset:x_offset + valid_block.shape[0],
-        y_offset:y_offset + valid_block.shape[1],
-        z_offset:z_offset + valid_block.shape[2]
+        x_offset : x_offset + valid_block.shape[0],
+        y_offset : y_offset + valid_block.shape[1],
+        z_offset : z_offset + valid_block.shape[2],
     ] = valid_block
 
     return padded_block
+
 
 def chunk_index_for_point(seg_point):
     """
@@ -188,24 +193,29 @@ def lookup_ids(seg_point: Vec3D):
     our graphene volume.  Return both the supervoxel ID and the root ID.
     """
     # pylint: disable-next=global-statement
+    assert seg_vol is not None
     sv_id = seg_vol[seg_point[0], seg_point[1], seg_point[2]].item()
     root_id = seg_vol.get_roots(sv_id).item()
     return sv_id, root_id
 
 
-def get_id_blocks(center_point: Vec3D, size: Vec3D=Vec3D(256,256,8)):
+def get_id_blocks(center_point: Vec3D, size: Vec3D = Vec3D(256, 256, 8)):
     """
     Return a 3D block of supervoxel IDs and root IDs around the given point.
     """
     half_size = size / 2
     start_pos = floor(center_point - half_size)
     end_pos = floor(center_point + half_size)
-    sv_block = safe_get_block(seg_vol, 
-                              slice(start_pos[0], end_pos[0]), 
-                              slice(start_pos[1], end_pos[1]),
-                              slice(start_pos[2], end_pos[2]))
+    assert seg_vol is not None
+    sv_block = safe_get_block(
+        seg_vol,
+        slice(start_pos[0], end_pos[0]),
+        slice(start_pos[1], end_pos[1]),
+        slice(start_pos[2], end_pos[2]),
+    )
     root_block = seg_vol.get_roots(sv_block.reshape(-1)).reshape(sv_block.shape)
     return sv_block, root_block
+
 
 def most_common_nonzero(data: np.ndarray, return_count=False):
     """
@@ -233,6 +243,7 @@ def most_common_nonzero(data: np.ndarray, return_count=False):
             return None, 0
         return None
 
+
 def touches_XY_edge_of_block(value, data_block):
     """
     Return whether the given value is found on any X or Y face of the given block of data.
@@ -253,8 +264,9 @@ def touches_XY_edge_of_block(value, data_block):
 def abbreviate(long_id, max_length=11):
     s = str(long_id)
     if len(s) > max_length:
-        s = s[:max_length//2] + "…" + s[-max_length//2:]
+        s = s[: max_length // 2] + "…" + s[-max_length // 2 :]
     return s
+
 
 def process_line(line: str):
     """
@@ -270,7 +282,7 @@ def process_line(line: str):
     if point is None:
         return
     print(f"\n{tuple(point)} ...")
-    point *= inp_to_seg   # convert to segmentation coordinates
+    point *= inp_to_seg  # convert to segmentation coordinates
     size = Vec3D(512, 512, 8)
     sv_block, root_block = get_id_blocks(point, size)
     center = tuple(s // 2 for s in size)
@@ -279,27 +291,31 @@ def process_line(line: str):
         print(f"Got 0 central root ID; skipping")
         return
     if touches_XY_edge_of_block(central_root_id, root_block):
-        print(f"{central_root_id} at {round(point/inp_to_seg)} extends beyond data window; skipping")
+        print(
+            f"{central_root_id} at {round(point/inp_to_seg)} extends beyond data window; skipping"
+        )
         return
     print(f"central_root_id = {central_root_id}")
-    dpos = floor(point - size/2)
-    point_offset = Vec3D(0, 0, -0.4) # (to work around NG's off-by-0.5-in-Z issue)
+    dpos = floor(point - size / 2)
+    point_offset = Vec3D(0, 0, -0.4)  # (to work around NG's off-by-0.5-in-Z issue)
 
-    mask = (root_block == central_root_id)
+    mask = root_block == central_root_id
     dilated_mask = scipy.ndimage.binary_dilation(mask)
     border_mask = dilated_mask & ~mask
     border_roots = root_block[border_mask]
     neighbor_root_id, neighbor_count = most_common_nonzero(border_roots, return_count=True)
     total_neighbors = border_mask.sum()
     percent_covered = round(100 * neighbor_count / total_neighbors)
-    print(f"{total_neighbors} total neighbors; biggest is {neighbor_root_id} with {neighbor_count} ({percent_covered}%)")
+    print(
+        f"{total_neighbors} total neighbors; biggest is {neighbor_root_id} with {neighbor_count} ({percent_covered}%)"
+    )
+    assert args is not None
     threshold = int(args.pct)
     if percent_covered < threshold:
         print(f"That's not dominant enough ({percent_covered} < {threshold}).  Bailing out.")
         if args.anno:
             point_annotations.append(
-                "{" +
-                f'"point":{list(round(point / inp_to_seg + point_offset))},'
+                "{" + f'"point":{list(round(point / inp_to_seg + point_offset))},'
                 f'"description":"{neighbor_root_id}: {percent_covered} < {threshold}",'
                 '"type":"point",'
                 f'"id":"{central_root_id}"'
@@ -315,32 +331,42 @@ def process_line(line: str):
     border_mask = border_mask[:, :, z]
     neighbor_sv_id = most_common_nonzero(sv_slice[(root_slice == neighbor_root_id) & border_mask])
     neighbor_pos = interior_point(sv_slice == neighbor_sv_id)
-    print(f'Border neighbor sv: {neighbor_sv_id}, with center at {neighbor_pos}')
+    print(f"Border neighbor sv: {neighbor_sv_id}, with center at {neighbor_pos}")
 
-    neighbor_sv_mask = (sv_slice == neighbor_sv_id)
-    neighbor_border_mask = scipy.ndimage.binary_dilation(neighbor_sv_mask, iterations=2) & ~neighbor_sv_mask
+    neighbor_sv_mask = sv_slice == neighbor_sv_id
+    neighbor_border_mask = (
+        scipy.ndimage.binary_dilation(neighbor_sv_mask, iterations=2) & ~neighbor_sv_mask
+    )
     proximal_svs = sv_slice[(root_slice == central_root_id) & neighbor_border_mask]
     if proximal_svs.any():
         central_sv_id = most_common_nonzero(proximal_svs)
-        print(f'A central supervoxel next to that is {central_sv_id}')
+        print(f"A central supervoxel next to that is {central_sv_id}")
     else:
         central_sv_id = sv_slice[center[0], center[1]]
         print(f"WTF? Couldn't find any nearby central SVs; using {central_sv_id} at input point")
     central_pos = interior_point(sv_slice == central_sv_id)
-    print(f'And THAT has a center at {central_pos}, with SV {sv_slice[central_pos]} and root {root_slice[central_pos]}')
+    print(
+        f"And THAT has a center at {central_pos}, with SV {sv_slice[central_pos]} and root {root_slice[central_pos]}"
+    )
 
     # Calculate our endpoints, in input coordinates
-    neighbor_point = round(Vec3D(dpos[0] + neighbor_pos[0], dpos[1] + neighbor_pos[1], point[2]) / inp_to_seg)
-    central_point = round(Vec3D(dpos[0] + central_pos[0], dpos[1] + central_pos[1], point[2]) / inp_to_seg)
-    print(f'Merge line: {tuple(neighbor_point)} - {tuple(central_point)}')
-    
+    assert neighbor_pos is not None
+    assert central_pos is not None
+    neighbor_point = round(
+        Vec3D(dpos[0] + neighbor_pos[0], dpos[1] + neighbor_pos[1], point[2]) / inp_to_seg
+    )
+    central_point = round(
+        Vec3D(dpos[0] + central_pos[0], dpos[1] + central_pos[1], point[2]) / inp_to_seg
+    )
+    print(f"Merge line: {tuple(neighbor_point)} - {tuple(central_point)}")
+
+    assert args is not None
     if not args.dry_run:
         do_merge(central_sv_id, central_point, neighbor_sv_id, neighbor_point)
 
     if args.anno:
         line_annotations.append(
-            "{" +
-            f'"pointA":{list(central_point + point_offset)},'
+            "{" + f'"pointA":{list(central_point + point_offset)},'
             f'"pointB":{list(neighbor_point + point_offset)},'
             f'"description":"{abbreviate(central_root_id)} - {abbreviate(neighbor_root_id)}",'
             '"type":"line",'
@@ -348,40 +374,52 @@ def process_line(line: str):
             "}"
         )
 
+
 def do_merge(sv1: int, point1: Vec3D, sv2: int, point2: Vec3D):
-    global cg_client, pcg_table_name
+    global client, cg_client, pcg_table_name
+    assert client is not None
     if cg_client is None:
         cg_client = chunkedgraph.ChunkedGraphClient(
-            server_address='https://data.proofreading.zetta.ai', 
-            table_name=pcg_table_name, 
-            auth_client=client.auth
+            server_address="https://data.proofreading.zetta.ai",
+            table_name=pcg_table_name,
+            auth_client=client.auth,
         )
     print(f"Merging SVs {sv1}-{sv2}, at points {point1}-{point2}, in {pcg_table_name}")
     cg_client.do_merge((sv1, sv2), (point1, point2), point_resolution)
 
+
 def process_stdin():
     for line in sys.stdin:
         process_line(line.strip())
-        
+
+
 def main():
     global args, engine, base_table, supervox_table, point_resolution
     global seg_vol, seg_bounds, seg_resolution, seg_chunk_size, seg_offset
     global inp_to_seg, line_annotations, point_annotations, pcg_table_name
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--resolution", help="resolution of point coordinates", required=True)
+    parser.add_argument(
+        "-r", "--resolution", help="resolution of point coordinates", required=True
+    )
     parser.add_argument("--seg", help="segmentation volume path", required=True)
     parser.add_argument("--sv", help="supervoxel volume path")
-    parser.add_argument("--pct", default="90", help="percent of border which must be touched to merge")
-    parser.add_argument("--dry-run", action="store_true", help="if true, print merges without actually doing them")
-    parser.add_argument("--anno", action="store_true", help="if true, output lines/points representing the results")
+    parser.add_argument(
+        "--pct", default="90", help="percent of border which must be touched to merge"
+    )
+    parser.add_argument(
+        "--dry-run", action="store_true", help="if true, print merges without actually doing them"
+    )
+    parser.add_argument(
+        "--anno", action="store_true", help="if true, output lines/points representing the results"
+    )
     args = parser.parse_args()
     if args.dry_run:
         print("Dry Run mode active.")
 
-    pcg_table_name = args.seg.split('/')[-1]
-    print(f'PCG table: {pcg_table_name}')
-    point_resolution = parse_point(args.resolution)
+    pcg_table_name = args.seg.split("/")[-1]
+    print(f"PCG table: {pcg_table_name}")
+    point_resolution = parse_point(args.resolution)  # type: ignore
     print(f"Input point resolution: {tuple(point_resolution)}")
 
     # HACK for testing:
@@ -389,7 +427,7 @@ def main():
     # sys.exit()
 
     print()
-    seg_path = args.seg #input("Segmentation volume path: ")
+    seg_path = args.seg  # input("Segmentation volume path: ")
     seg_vol, seg_bounds = load_volume(seg_path, 1)
     seg_resolution = Vec3D(*seg_vol.resolution)
     seg_chunk_size = Vec3D(*seg_vol.chunk_size)
@@ -409,7 +447,9 @@ def main():
         print(f"\n{len(line_annotations)} merge lines, in resolution {tuple(point_resolution)}:\n")
         print(",\n".join(line_annotations))
 
-        print(f"\n{len(point_annotations)} failed merges, in resolution {tuple(point_resolution)}:\n")
+        print(
+            f"\n{len(point_annotations)} failed merges, in resolution {tuple(point_resolution)}:\n"
+        )
         print(",\n".join(point_annotations))
 
 

145bc2e78c0edb8c8f114f7ac536196e6230b9b1
JoeStrout <joe@strout.net>
2025-05-19 22:09:20 +0000

    Fix recursive import error, and remove just-added imports since they appear redundant.

--- zetta_utils/layer/volumetric/annotation/__init__.py
+++ zetta_utils/layer/volumetric/annotation/__init__.py
@@ -1,4 +1,5 @@
 from .backend import AnnotationLayerBackend, LineAnnotation
 from .layer import VolumetricAnnotationLayer
 from .build import build_annotation_layer
-from .tasks import post_process_annotation_layer_op, post_process_annotation_layer_flow
+
+# from .tasks import post_process_annotation_layer_op, post_process_annotation_layer_flow

c7f4f62429492a34c43586aa2a06794324b38e53
JoeStrout <joe@strout.net>
2025-05-28 17:40:04 +0000

    Restore standard typechecking in annotation_postprocessing.py.

--- zetta_utils/mazepa_layer_processing/annotation_postprocessing.py
+++ zetta_utils/mazepa_layer_processing/annotation_postprocessing.py
@@ -3,18 +3,15 @@ Module containing task and flow definitions for annotation layer operations.
 """
 
 from zetta_utils import builder, mazepa
-
-# from zetta_utils.layer.volumetric import VolumetricAnnotationLayer
+from zetta_utils.layer.volumetric import VolumetricAnnotationLayer
 
 
 @mazepa.taskable_operation
-def post_process_annotation_layer_op(target):  # pragma: no cover
-    # target: VolumetricAnnotationLayer
+def post_process_annotation_layer_op(target: VolumetricAnnotationLayer):  # pragma: no cover
     target.backend.post_process()
 
 
 @builder.register("post_process_annotation_layer_flow")
 @mazepa.flow_schema
-def post_process_annotation_layer_flow(target):  # pragma: no cover
-    # target: VolumetricAnnotationLayer
+def post_process_annotation_layer_flow(target: VolumetricAnnotationLayer):  # pragma: no cover
     yield post_process_annotation_layer_op.make_task(target)

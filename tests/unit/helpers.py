f9bbb9835664009f75c63377b7d6a0ebce79ba49
Nico Kemnitz <nico@zetta.ai>
2025-09-19 10:34:59 +0200

    feat(interpolate): Expose mask_thr_val and align_corners kwargs

--- tests/unit/helpers.py
+++ tests/unit/helpers.py
@@ -12,3 +12,13 @@ def assert_array_equal(a, b):
         assert torch.equal(a, b)
     else:
         raise ValueError(f"Invalid input type '{type(a)}'")
+
+
+def assert_array_almost_equal(a, b, decimal=6):
+    """Generic assertion for approximate equality testing."""
+    if isinstance(a, np.ndarray):
+        np.testing.assert_array_almost_equal(a, b, decimal=decimal)
+    elif isinstance(a, torch.Tensor):
+        assert torch.allclose(a, b, atol=10 ** (-decimal))
+    else:
+        raise ValueError(f"Invalid input type '{type(a)}'")

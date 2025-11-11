aabb906e07fe8fdbe3d066f6f7cd70dfc34cf45a
Dodam Ih <dodam@zetta.ai>
2025-11-11 15:39:52 -0800

    bye

--- zetta_utils/cloud_management/resource_allocation/gcloud/artifact_registry.py
+++ zetta_utils/cloud_management/resource_allocation/gcloud/artifact_registry.py
@@ -22,8 +22,9 @@ def check_image_exists(image: str) -> bool:
         )
         return False
 
-    credentials, _ = default()
-    credentials.refresh(Request())
+    credentials, _ = default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    if not credentials.valid:
+        credentials.refresh(Request())
     service = build("artifactregistry", "v1", credentials=credentials)
     tag_name = (
         f"projects/{project_id}/locations/{location}/"

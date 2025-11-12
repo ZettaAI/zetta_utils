31ade11366fd17a6a0cdbdd8b6e06e64174dbc9b
zettavasyl <vasyl.perehinets@zetta.ai>
2025-11-12 16:10:53 +0200

    feat: :sparkles: add GPU alignment Docker file version & optimise response with base64

---
+++ build_image_web_gpu.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# pylint: disable=missing-docstring,line-too-long
+import argparse
+import subprocess
+
+BUILD_COMMAND_TMPL = "docker build --platform linux/amd64 --network=host -t {REGION}-docker.pkg.dev/{PROJECT}/{REPO}/web_api_gpu:{TAG} -f web_api/Dockerfile.gpu ."
+PUSH_COMMAND_TMPL = "docker push {REGION}-docker.pkg.dev/{PROJECT}/{REPO}/web_api_gpu:{TAG}"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Build and push GPU docker image.")
+    parser.add_argument("--project", type=str, required=True, help="GCR project.")
+    parser.add_argument("--tag", "-t", type=str, required=True, help="Image tag name/version.")
+    parser.add_argument("--region", type=str, default="us-east1", help="Artifact Registry region.")
+    parser.add_argument("--repo", type=str, default="zutils", help="Artifact Registry repo name.")
+
+    args = parser.parse_args()
+
+    build_command = BUILD_COMMAND_TMPL
+    build_command = build_command.replace("{TAG}", args.tag)
+    build_command = build_command.replace("{PROJECT}", args.project)
+    build_command = build_command.replace("{REGION}", args.region)
+    build_command = build_command.replace("{REPO}", args.repo)
+    print(f"Running: \n{build_command}")
+    subprocess.call(build_command, shell=True)
+
+    push_command = PUSH_COMMAND_TMPL
+    push_command = push_command.replace("{TAG}", args.tag)
+    push_command = push_command.replace("{PROJECT}", args.project)
+    push_command = push_command.replace("{REGION}", args.region)
+    push_command = push_command.replace("{REPO}", args.repo)
+    print(f"Running: \n{push_command}")
+    subprocess.call(push_command, shell=True)
+
+    print(f"\nAdding git tag {args.tag}_gpu.")
+    subprocess.call(f"git tag {args.tag}_gpu && git push origin {args.tag}_gpu", shell=True)
+
+
+if __name__ == "__main__":
+    main()

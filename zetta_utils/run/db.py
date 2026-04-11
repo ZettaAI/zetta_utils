f5bbab7ff34b73c9596edac234adc227d9ef561a
Akhilesh Halageri <akhilesh@zetta.ai>
2026-04-11 03:20:26 +0000

    chore: remove legacy GCS_STATS_DB and aggregate_gcs_stats
    The pod-stats collection and aggregate_pod_stats fully supersede the
    old gcs-stats-proxy collection. Drops the orphaned function, the
    import, and the unused Firestore layer definition.

--- zetta_utils/run/db.py
+++ zetta_utils/run/db.py
@@ -3,7 +3,6 @@ from zetta_utils.layer.db_layer.firestore import build_firestore_layer
 
 RUN_COLLECTION = "run-info"
 NODE_COLLECTION = "node-info"
-GCS_STATS_COLLECTION = "gcs-stats-proxy"
 POD_STATS_COLLECTION = "pod-stats"
 
 
@@ -13,10 +12,6 @@ NODE_DB = build_firestore_layer(
 
 RUN_DB = build_firestore_layer(
     RUN_COLLECTION, database=constants.RUN_DATABASE, project=constants.DEFAULT_PROJECT
-)
-
-GCS_STATS_DB = build_firestore_layer(
-    GCS_STATS_COLLECTION, database=constants.RUN_DATABASE, project=constants.DEFAULT_PROJECT
 )
 
 POD_STATS_DB = build_firestore_layer(
